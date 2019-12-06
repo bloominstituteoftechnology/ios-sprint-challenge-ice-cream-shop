2c7885fc07fb1b25231593a3793303e861e17f77
Stephanie Ballard <user.name>
2019-12-06 08:51:08 -0800

    saving progress

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,30 @@
+//Stephanie Ballard
 
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+
+struct Flavor {
+    let name: String
+    let rating: String
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size: Size
+}
+
+func eat() {
+    print("Mmm! I love \(Flavor.self)")
+}
+
+class IceCreamShop {
+    var flavor: String
+    var size: String
+    var topping: String
+    var totalSales: Double
+}
