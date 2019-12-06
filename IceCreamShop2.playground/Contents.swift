273ecdcd0b9629a3576733ef04029188aa3a59cd
Benjamin LeMaine <ben.lemaine@gmail.com>
2019-12-06 14:23:45 -0500

    added ice cream sprint challenge

---
+++ IceCreamShop2.playground/Contents.swift
@@ -0,0 +1,35 @@
+struct Flavor {
+   var  name: String
+   var rating: String
+}
+
+enum Size  {
+    case small
+    case medium
+    case large
+}
+
+
+struct Cone {
+    var flavor: String = "Vanilla"
+    var topping: String
+    var coneSize: String
+}
+
+
+
+func eat() {
+    print("Mmm! I Love \(flavor)")
+}
+
+
+class IceCreamShop {
+    var Vanilla
+    var flsvor2: String
+    var  totalSales: Int
+}
+
+
+func listTopFlaver() {
+    
+}
