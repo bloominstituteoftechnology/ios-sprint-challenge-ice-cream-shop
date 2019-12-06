aee78153b9a8afbb16e470c5b05c81a4932bfdf7
Rohan Taylor <rhntaylor@gmail.com>
2019-12-06 08:38:49 -0800

    halfwayDone

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
+struct Flavor {
+    var name : String
+    var rating : String
+}
 
+enum Size : Double {
+    case small
+    case medium
+    case large
+}
+
+struct Cone {
+    var flavor : Flavor
+    var topping : String
+    var size : Size
+    
+    func eat() {
+        print("Mmm! I love!")
+    }
+}
+
+class iceCreamShop {
+    var menu : Cone
+    var totalSales : Double
+    init(flavor : Flavor, topping: String,size:Size, totalSales : Double) {
+        self.totalSales = totalSales
+        self.menu = Cone(flavor: Flavor, topping: String, size: Size)
+       
+    }
+    func listTopFlavors() {
+        
+    }
+}
