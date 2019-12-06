9c4277b108ae5f1af0bd32e6b07e5c11aa44e2c6
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-06 17:15:32 +0000

    Steps 5 and 6 complete

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -8,12 +8,12 @@ enum Size: Double {
 
 struct Flavor {
     var name: String
-    var rating: Int
+    var rating: Double
 }
 
 struct Cone {
     let flavor: Flavor
-    let topping: String
+    let topping: String?
     let size: Size
     
     func eat() {
@@ -25,12 +25,40 @@ class IceCreamShop {
     var flavors: [Flavor]
     var sizes: [Size]
     var toppings: [String]
-    var totalSales: Double?
+    var totalSales: Double
     
-    init(flavors: [Flavor], sizes: [Size], toppings: [String]) {
+    init(flavors: [Flavor], sizes: [Size], toppings: [String], totalSales: Double) {
         self.flavors = flavors
         self.sizes = sizes
         self.toppings = toppings
+        self.totalSales = totalSales
     }
     
+    func listTopFlavors() {
+        var topFlavors = String()
+        
+        for flavor in flavors {
+            if flavor.rating > 4.0 {
+                topFlavors.append(flavor.name)
+            }
+        }
+        
+        print("Our top flavors are \(topFlavors)")
+    }
+    
+    func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone? {
+        let cone = Cone(flavor: flavor, topping: topping, size: size)
+        totalSales = cone.size.rawValue
+        
+        if let topping = topping {
+            print("Your \(flavor) ice cream with \(topping) is \(totalSales)")
+        } else {
+            print("Your \(flavor) ice cream is \(totalSales)")
+        }
+        
+        return cone
+    }
 }
+
+
+
