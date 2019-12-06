0c5fd63a5625bd425566f4bc292dc0f3f8a2199f
Stephanie Ballard <user.name>
2019-12-06 09:33:26 -0800

    updating progress second hour

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -9,7 +9,7 @@ enum Size: Double {
 
 struct Flavor {
     let name: String
-    let rating: String
+    let rating: Double
 }
 
 struct Cone {
@@ -17,6 +17,10 @@ struct Cone {
     let topping: String
     let size: Size
 }
+
+let flavor: String, rating = ["Vanilla Marshmellow": 3.0, "Chocolate Cookie Crunch": 5.0 , "Srawberry Bliss": 4.0, "Peanutbutter Swirl": 5.0, "Chocolate Chip Mint": 4.0, "Cookie Dough": 4.0, "Oreo": 5.0, "Rocky Road": 3.0, "Christmas Peppermintbark": 4.0]
+
+let topFlavors = ["Chocolate Cookie Crunch", "Peanutbutter Swirl", "Oreo"]
 
 func eat() {
     print("Mmm! I love \(Flavor.self)")
@@ -27,4 +31,19 @@ class IceCreamShop {
     var size: String
     var topping: String
     var totalSales: Double
+    
+    init(flavor: String, size: String, topping:String) {
+        self.flavor = flavor
+        self.size = size
+        self.topping = topping
+        self.totalSales = totalSales
+    }
+}
+
+
+func listTopFlavors() {
+    let topRatedFlavors =
+    {
+        print("Our top rated flavors are ")
+    }
 }
