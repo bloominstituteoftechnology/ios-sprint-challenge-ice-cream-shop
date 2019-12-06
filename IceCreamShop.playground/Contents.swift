d2441268ccf25202dfed9ae1fdc7fbcff84c64c9
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-06 16:51:04 +0000

    Steps 1-4 of project complete

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,36 @@
+// Tobi Tselios
 
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Flavor {
+    var name: String
+    var rating: Int
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Mmm! I love this \(flavor) cone.")
+    }
+}
+
+class IceCreamShop {
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: [String]
+    var totalSales: Double?
+    
+    init(flavors: [Flavor], sizes: [Size], toppings: [String]) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+    }
+    
+}
