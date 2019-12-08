f5995c97440249c9a0ec5a829838dbeba572cc5a
Malik <malik21barnes@gmail.com>
2019-12-08 15:32:09 -0800

    Finished Sprint Challenge

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,17 +1,7 @@
 // Malik Barnes
 
-enum Flavors: String {
-     case chocolateChip = "Chocolate Chip"
-     case mintChocolateChip = "Mint Chocolate Chip"
-     case rainbowSherbet = "Rainbow Sherbet"
-     case chocolateChipCookieDough = "Chocolate Chip Cookie Dough"
-     case vanilla = "Vanilla"
-     case chocolate = "Chocolate"
-     case strawberry = "Strawberry"
-}
-
 struct Flavor {
-    var name: Flavors
+    var name: String
     var rating: UInt
 }
 
@@ -21,50 +11,78 @@ enum Size: Double {
     case large = 2.99
 }
 
+struct Cone {
+    let flavor: Flavor
+    let size: Size
+    
+    func eat() {
+        print("Wow! I love \(flavor.name)!")
+    }
+}
+
+let chocolateChip = Flavor(name: "Chocolate Chip", rating: 4)
+let mintChocolateChip = Flavor(name: "Mint Chocolate Chip", rating: 7)
+let rainbowSherbet = Flavor(name: "Rainbow Sherbet", rating: 1)
+let  chocolateChipCookieDough = Flavor(name: "Chocolate Chip Cookie Dough", rating: 9)
+let vanilla = Flavor(name: "Vanilla", rating: 3)
+let chocolate = Flavor(name: "Chocolate", rating: 4)
+let strawberry = Flavor(name: "Strawberry", rating: 6)
+
 enum Toppings: String {
     case sprinkles = "Sprinkles"
     case oreoCrumbles = "Oreo Crumbles"
     case hotFudge = "Hot Fudge"
     case caramel = "Caramel"
     case whippedCream = "Whipped Cream"
-}
-
-struct Cone {
-    let flavor: Flavors
-    let toppings: Toppings?
-    let size: Size
-    
-    func eat() {
-        print("Wow! I love \(self.flavor.rawValue)!")
-    }
+    case brownieBites = "Brownie Bites"
 }
 
 class IceCreamShop {
-    var menu: Flavors
-    var coneSize: Size
-    var toppings: Toppings
+    var flavors: [Flavor]
+    var coneSize: [Size]
     
-    init(menu: Flavors, coneSize: Size, toppings: Toppings) {
-        self.menu = menu
+    init(flavors: [Flavor], coneSize: [Size]) {
+        self.flavors = flavors
         self.coneSize = coneSize
-        self.toppings = toppings
     }
-}
 
+   func listTopFlavors() {
+    var topFlavorsList = [Flavor]()
+    
+    for flavor in flavors {
+        if flavor.rating >= 4 {
+            topFlavorsList.append(flavor)
+        }
+       }
+    print("Our top flavors are \(topFlavorsList)")
+           // I couldnt figure out how to make it just print out the array of flavor names
+   }
+    
+   var totalSales: Double = 0
 
-var totalSales: Double = 0
+    func orderCone(size: Size,flavor: Flavor, toppings: Toppings? ) -> Cone? {
+        let newCone = Cone(flavor: flavor, size: size)
+        totalSales += size.rawValue
 
-func orderCone(_ cone: Cone) -> Cone? {
-    let newCone = cone
-    totalSales += cone.size.rawValue
-
-    if let unwrappedTopping = newCone.toppings{
-        print("Your \(newCone.flavor.rawValue) with \(unwrappedTopping.rawValue) is going to be \(cone.size.rawValue)")
-    } else if newCone.toppings == nil {
-        print("Your \(newCone.flavor.rawValue) cone is going to be \(newCone.size.rawValue)")
+        if let unwrappedTopping = toppings{
+            print("Your \(newCone.flavor.name) with \(unwrappedTopping.rawValue) is going to be \(newCone.size.rawValue)")
+        } else if toppings == nil {
+            print("Your \(newCone.flavor.name) cone is going to be \(size.rawValue)")
+        }
+        return newCone
     }
-    return newCone
+    
+    let differentSizes: [Size] = [.small, .medium, .large]
+    
+    let differentToppings: [Toppings] = [.brownieBites, .caramel, .hotFudge, .oreoCrumbles, .sprinkles, .whippedCream]
 }
+ 
 
-let newCone = orderCone(Cone(flavor: .chocolateChipCookieDough, toppings: .hotFudge, size: .large))
-newCone
+let theIceCreamShop = IceCreamShop(flavors: [chocolate, chocolateChip, mintChocolateChip, rainbowSherbet, chocolateChipCookieDough, vanilla, strawberry], coneSize: [.small, .medium, .large])
+
+theIceCreamShop.listTopFlavors()
+ 
+let newConeOrder = theIceCreamShop.orderCone(size: .large, flavor: chocolateChipCookieDough, toppings: .brownieBites)
+newConeOrder?.eat()
+
+
