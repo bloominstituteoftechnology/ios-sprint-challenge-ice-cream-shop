8fa4f967f83dec61b5a5e43a2bfaed59c786067e
Rebecca <mythreldrows@aol.com>
2020-01-17 12:06:50 -0500

    all work up to 5

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,49 @@
+ struct Flavor {
+    var name: String
+    var rating: Double
+ }
+ 
+ enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+ }
 
+ 
+ struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+ 
+
+ func eat() {
+    print("Mmm! I love \(flavor.name)")
+  }
+ }
+ 
+ class IceCreamShop {
+    let flavor: [Flavor]
+    let size: [Size]
+    let toppings: [String]
+    var totalSales: Double
+    
+    init(flavor: [Flavor], size: [Size], toppings: [String], totalSales: Double) {
+        self.flavor = flavor
+        self.size = size
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+ }
+ 
+ func listTopFlavors() {
+   
+    print("Our top flav
+     for flavor in IceCreamShop {
+        if Flavor.rating > 4 {
+            print("\(flavor.name)")
+            return
+        } else {
+            return
+        }
+    }
+ }
