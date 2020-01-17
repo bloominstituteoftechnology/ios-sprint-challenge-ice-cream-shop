6335e87916851e9eaf69f08d5064d8acb8257185
Keri Levesque <kerianne116@gmail.com>
2020-01-17 11:18:20 -0500

    created struct Flavor, enum Size, struct Cone and func eat

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
+// 1. Create a struct called flavor. Add the follow
+
+struct Flavor {
+    
+    let name: String
+    let rating: Int
+}
+
+// 2. Create a enum called Size
+
+enum Size: Double {
+    
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+// 3. Create a struct called Cone. Create a function called eat. No arguments. It should print a string of text that include the name of the cone's flavor.
+
+struct Cone {
+    
+    let flavor: String
+    let topping: String
+    let size: Size  // referring to enum
+    
+    func eat() {
+        
+        print("Mmm! I love \(self.flavor)")
+    }
+
+}
+
 
