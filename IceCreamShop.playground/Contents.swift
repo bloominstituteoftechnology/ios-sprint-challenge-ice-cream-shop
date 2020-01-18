5c840f91f77870c6ffd919f4dca6255c54fadfb2
Nichole Davidson <nicholedavid@gmail.com>
2020-01-18 15:30:07 -0700

    Added better code

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -2,15 +2,12 @@
 struct Flavor {
     var name: String
     var rating: Double
-    
 }
-
 
 enum Size: Double {
     case small = 3.99
     case medium = 5.50
     case large = 6.99
-    
 }
 
 struct Cone {
@@ -24,34 +21,36 @@ struct Cone {
 }
 
 class IceCreamShop {
-    let flavorOptions: [String]
+    var flavors: [Flavor]
     let size: Size
     var toppings: String?
     var totalSales: Double
     
-    init(flavorOptions: String, size: Size, toppings: String, totalSales: Double) {
-        self.flavorOptions = []
-        self.size = size
-        self.toppings = toppings
-        self.totalSales = totalSales
+    init(name: String, rating: Double) {
+        self.flavors = []
+      
     }
 }
 
-var coocooChocolate = 4.2
-var millaVanilla = 3.8
-var strawberryShorts = 4.6
-var circusCandy = 3.6
-var pastachioDisguisey = 5.0
+var chocolate = Flavor(name: "Coocoo Chocolate", rating: 4.2)
+var vanilla = Flavor(name: "Milla Vanilla", rating: 3.8)
+var strawberry = Flavor(name: "Strawberry Shorts", rating: 4.6)
+var cottonCandy = Flavor(name: "Circus Candy", rating: 3.6)
+var pastachio = Flavor(name: "Pastachio Disguisey", rating: 5.0)
 
-let flavors: [Double] = [coocooChocolate, millaVanilla, strawberryShorts, circusCandy, pastachioDisguisey]
+var flavoroptions = IceCreamShop(name: "Coocoo Chocolate", rating: 4.2)
+
+flavoroptions.flavors.append(vanilla)
+flavoroptions.flavors.append(strawberry)
+flavoroptions.flavors.append(cottonCandy)
+flavoroptions.flavors.append(pastachio)
+
 
 func listTopFlavors() {
-    for ratings in flavors {
-        switch ratings {
-        case 4.0 ... 5.0:
-            print("Our top flavors are \(flavors)")
-        default:
-            continue
+    for ratings in flavoroptions {
+        switch ratings.rating {
+    case 4.0 ... 5.0:
+        print("Our top flavors are \(flavors)")
         }
     }
 }
