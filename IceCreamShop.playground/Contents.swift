b916b7bca36582ddb4e89c3b89d9d99d20de42b3
Rebecca <mythreldrows@aol.com>
2020-01-18 12:01:02 -0500

    I think this is finished.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -27,8 +27,8 @@
     var toppings: [String]
     var totalSales: Double
     
-    init(flavor: [Flavor], size: [Size], toppings: [String], totalSales: Double) {
-        self.flavors = flavor
+    init(flavors: [Flavor], size: [Size], toppings: [String], totalSales: Double) {
+        self.flavors = flavors
         self.size = size
         self.toppings = toppings
         self.totalSales = totalSales
@@ -37,8 +37,8 @@
     func listTopFlavors() {
         var topFlavors: [String] = []
         
-       for flavor in flavors {
-           if flavor.rating > 4 {
+        flavors.forEach { flavor in
+           if flavor.rating >= 4 {
             topFlavors.append("\(flavor.name)" + ", ")
             return
            } else {
@@ -47,17 +47,17 @@
         }
          print("Our top flavors are \(topFlavors).")
     }
- }
- 
  func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone? {
-    let newCone: Cone = Cone(flavor: flavor, topping: topping!, size: size)
+    let newCone: Cone = Cone(flavor: flavor, topping: topping, size: size)
     
     if let unwrappedTopping = topping {
         print("Your \(flavor.name) ice cream with \(unwrappedTopping) is $\(size.rawValue).")
         } else {
         print("Your \(flavor.name) ice cream is $\(size.rawValue)")
     }
+    totalSales += size.rawValue
      return newCone
+ }
  }
  
  let chocolate = Flavor(name: "Chocolate", rating: 5)
@@ -65,7 +65,11 @@
  let strawberry = Flavor(name: "Strawberry", rating: 4)
  let nutella = Flavor(name: "Nutella", rating: 5)
 
- var beckysIceCreamShop = IceCreamShop(flavor: [chocolate, vanilla, strawberry, nutella], size: [.large, .small, .medium], toppings: ["chocolate shell", "sprinkles", "caramel drizzle"], totalSales: 0)
+ var beckysIceCreamShop = IceCreamShop(flavors: [chocolate, vanilla, strawberry, nutella], size: [.large, .small, .medium], toppings: ["chocolate shell", "sprinkles", "caramel drizzle"], totalSales: 0)
  
  beckysIceCreamShop.listTopFlavors()
+ 
+ beckysIceCreamShop.orderCone(flavor: strawberry, topping: "chocolate shell", size: .small)
+ print(beckysIceCreamShop.totalSales)
+
  
