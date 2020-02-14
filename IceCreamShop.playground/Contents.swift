925dd0bf3094de8ff0dcebcd21cad83f6331bf5c
Nichole Davidson <nicholedavid@gmail.com>
2020-02-14 11:48:19 -0700

    Stuck on step 9

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -21,37 +21,51 @@ struct Cone {
 }
 
 class IceCreamShop {
-    var flavors: [Flavor] = []
-    var sizes: [Size] = []
+    var flavors: [Flavor]
+    var sizes: [Size]
     var toppings: [String]
     var totalSales: Double
     
-    init(flavors: Flavor, sizes: Size, toppings: [String], totalSales: Double) {
-        self.flavors = [flavors]
-        self.sizes = [sizes]
-        self.toppings = []
+    init(flavors: [Flavor], sizes: [Size], toppings: [String], totalSales: Double) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
         self.totalSales = totalSales
     }
 
 func listTopFlavors() {
-    
     for flavor in flavors {
         if flavor.rating >= 4.0 {
             print("Our topflavors are \(flavor)")
       }
     }
   }
-}
-
+    
 func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone? {
     
     let newCone = Cone(flavor: flavor, topping: topping, size: size)
-    let newConePrice = newCone.size.rawValue
+    self.totalSales += newCone.size.rawValue
     
-    if topping != nil {
-        print("Your \(flavor) ice cream with \(topping) is \(size.rawValue)")
+    if let toppingChosen = newCone.topping {
+        print("Your \(flavor) ice cream with \(toppingChosen) is \(size.rawValue)")
     } else {
         print("Your \(flavor) ice cream is \(size.rawValue)")
     }
     return newCone
+ }
 }
+
+var vanillaBean = Flavor(name: "Slim Shady", rating: 3.5)
+var chocolateMousse = Flavor(name: "Biggie Smalls", rating: 4.6)
+var strawberryChocolate = Flavor(name: "Snoop Dogg", rating: 4.1)
+var pastachio = Flavor(name: "Childish Gambino", rating: 5.0)
+
+let flavorsOffered = [vanillaBean, chocolateMousse, strawberryChocolate, pastachio]
+let sizesOffered = [Size.small, Size.medium, Size.large]
+let toppingsOffered = ["Nuts", "Sprinkles", "Chocolate Syrup", "Caramel"]
+
+let newIceCreamShop = IceCreamShop(flavors: flavorsOffered, sizes: sizesOffered, toppings: toppingsOffered, totalSales: 0.0)
+
+newIceCreamShop.listTopFlavors()
+
+
