b909a0e4a4034a20e854d4f71dc32d2591779176
Shawn-James <shawn.james@me.com>
2020-02-14 11:56:46 -0500

    1st run-through of steps 1-12. Going back through now to make everything work

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -22,7 +22,7 @@ struct Cone {
     }
 }
 
-// 4-5.
+// 4-6.
 class IceCreamShop {
     var flavors: [Flavor]
     var sizes: Size
@@ -41,6 +41,41 @@ class IceCreamShop {
         }
        print(string)
     }
+    func orderCone(flavor: , topping: ?, size: ) -> Cone? {
+        let cone = Cone(flavor: flavor, topping: topping, size: size)
+        IceCreamShop.totalSales += .size.rawvalue
+        // Create a string that tells the price of the cone, along with its flavor and topping. NOTE: account for the potential lack of a topping on the Cone in that string by using optional binding (if-let). For example, the string could say "Your mint ice cream with chocolate chips is 3.99", or "Your vanilla ice cream is 5.99." Print the string.
+        return cone
+    }
 }
 
+// 7. At the bottom of the playground, create a few Flavor constants, an array of sizes, and an array of toppings.
 
+let vanilla = Flavor(name: "vanilla", rating: 2.5)
+let chocolate = Flavor(name: "chocolate", rating: 10.0)
+let strawberry = Flavor(name: "strawberry", rating: 3.5)
+let mango = Flavor(name: "mango", rating: 8.0)
+
+let availableSizes: [Size] = [Size.small, Size.medium, Size.large]
+
+let toppings: [String] = ["sprinkles", "chocolate sauce", "gummy bears"]
+
+// 8. Use the constants you just made to initialize a new IceCreamShop constant.
+
+let myShop = IceCreamShop()
+
+// 9. Call the shop's listTopFlavors function and make sure it runs correctly.
+
+myShop.listTopFlavors()
+
+// 10. Create a new Cone constant. Use the shop's orderCone function to assign the constant a Cone value.
+
+let myCone = myShop.orderCone()
+
+// 11. Using that new Cone constant, call its eat function without unwrapping the constant.
+
+myCone.eat()
+
+// 12. Print the shop's totalSales and make sure that it has increased since you ordered a cone in step 10. Validate your work through running your playground and ensure that your code operates as designed.
+
+print(myShop.totalSales)
