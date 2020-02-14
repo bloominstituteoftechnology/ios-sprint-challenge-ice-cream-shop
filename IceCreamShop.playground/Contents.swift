06d736afab7cd709feebfb3e5b09e7a6dbec7885
Lydia Zhang <art4everyan@gmail.com>
2020-02-14 11:17:52 -0500

    Put enum and struct

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,22 @@
+enum Size: Double {
+    case small = 3.99
+    case medium = 5.99
+    case large = 7.99
+}
+
+
+struct Flavor {
+    var name: String
+    var rating: Double
+}
+
+struct Cone {
+    var flavor: Flavor
+    var topping: String
+    var size: Size
+    
+    func eat() {
+        print("MMM! I love \(self.flavor).")
+    }
+}
 
