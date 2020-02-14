378ec45312f779454b0ed77f8150c60e1770d582
Shawn-James <shawn.james@me.com>
2020-02-14 11:26:49 -0500

    First commit

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
+// 1.
+struct Flavor {
+    var name: String
+    var rating: Double
+}
 
+// 2.
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+//// 3.
+//struct Cone {
+//    let flavor: Flavor
+//    let topping: String
+//    let size: Size
+//    
+//    func eat() {
+//        print("Mmm! I love \(self.flavor)!")
+//    }
+//}
+//
+//// 4.
+//class IceCreamShop {
+//    var flavors: [Flavor]
+//    var sizes: Size
+//    var toppings: [String]
+//    var totalSales: Double
+//}
+//
+//// 5.
