5d341606ca6cf249fb06cbaa64ea6b29a45b2059
Harmony Radley <harmonyradley99@gmail.com>
2020-02-14 11:46:00 -0500

    Completed steps 1-3

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
 
+
+struct Flavor {
+    var name: String
+    var rating: Double
+}
+
+
+enum Size: Double {
+    case small = 1.99
+    case medium = 2.99
+    case large = 3.99
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        
+        print("I love \(flavor) cone!")
+        
+    }
+    
+}
+
+
+class IceCreamShop {
+   
+    var totalSales: Double
+    
+}
