2abd585653e63f56546d7e1a2ab297e8fccab29b
fthmls <fthmls.one@gmail.com>
2020-02-14 13:10:28 -0500

    case insensitive order confirmation

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,7 +1,7 @@
 extension Cone {
     struct Flavor: Equatable {
         static func ==(lhs: Flavor, rhs: Flavor) {
-            lhs.name == rhs.name
+            lhs.name.lowercased() == rhs.name.lowercased()
         }
         let name: String
         let rating: Double
@@ -42,7 +42,6 @@ class IceCreamShop {
             print("Nobody seems to like any of our flavors.")
             return
         }
-        
         let formattedTopFlavors = topFlavors.map({ flavor -> String in
             if flavor == topFlavors.last { return "and \(flavor)." }
             else { return "\(flavor), " }
@@ -54,7 +53,7 @@ class IceCreamShop {
             print("Sorry, we don't have that flavor.")
             return nil
         }
-        guard toppings.allSatisfy(menu.toppings.contains) else {
+        guard toppings.map({$0.lowercased()}).allSatisfy(menu.toppings.map({$0.lowercased()}).contains) else {
             let grammar = toppings.count > 1 ? "those toppings" : "that topping"
             print("Sorry, we don't have \(grammar).")
             return nil
@@ -76,34 +75,36 @@ class IceCreamShop {
     var totalSales = 0.0
 }
 
-let newShopMenu: IceCreamShop.Menu = {
-    let newShopFlavors = [
-        Cone.Flavor(name: "Vanilla", rating: 5.0),
-        Cone.Flavor(name: "Chocolate", rating: 4.9),
-        Cone.Flavor(name: "Strawberry", rating: 4.0),
-        Cone.Flavor(name: "Raspberry", rating: 3.9),
-        Cone.Flavor(name: "Bacon", rating: 2.1),
-        Cone.Flavor(name: "Butter pecan", rating: 3.6),
-        Cone.Flavor(name: "Cookie dough", rating: 4.3),
-    ]
-    let newShopToppings = [
-    "Hot Fudge",
-    "Sprinkles",
-    "Caramel",
-    "Oreos",
-    "Peanut butter cups",
-    "Cookie dough",
-    "Whipped cream",
-    "Nuts",
-    "Fruit",
-    ]
-    let newShopSizes = [
-        Cone.Size.small,
-        Cone.Size.large
-    ]
-    return IceCreamShop.Menu(flavors: newShopFlavors, toppings: newShopToppings, sizes: newShopSizes)
+let iceCreamShop: IceCreamShop = {
+    let newShopMenu: IceCreamShop.Menu = {
+        let newShopFlavors = [
+            Cone.Flavor(name: "Vanilla", rating: 5.0),
+            Cone.Flavor(name: "Chocolate", rating: 4.9),
+            Cone.Flavor(name: "Strawberry", rating: 4.0),
+            Cone.Flavor(name: "Raspberry", rating: 3.9),
+            Cone.Flavor(name: "Bacon", rating: 2.1),
+            Cone.Flavor(name: "Butter pecan", rating: 3.6),
+            Cone.Flavor(name: "Cookie dough", rating: 4.3),
+        ]
+        let newShopToppings = [
+        "Hot Fudge",
+        "Sprinkles",
+        "Caramel",
+        "Oreos",
+        "Peanut butter cups",
+        "Cookie dough",
+        "Whipped cream",
+        "Nuts",
+        "Fruit",
+        ]
+        let newShopSizes = [
+            Cone.Size.small,
+            Cone.Size.large
+        ]
+        return IceCreamShop.Menu(flavors: newShopFlavors, toppings: newShopToppings, sizes: newShopSizes)
+    }()
+    return IceCreamShop(menu: newShopMenu)
 }()
-let iceCreamShop = IceCreamShop(menu: newShopMenu)
 
 iceCreamShop.listTopFlavors()
 
@@ -116,5 +117,6 @@ let cone: Cone? = {
     let selectedSize = iceCreamShop.menu.sizes.randomElement()!
     return iceCreamShop.orderCone(flavor: selectedFlavor, toppings: selectedToppings, size: selectedSize)
 }()
+
 cone?.eat()
 print("Total sales: \(iceCreamShop.totalSales)")
