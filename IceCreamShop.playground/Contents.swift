2e761a69953511aa93f5013c6ed020907af971a6
Diego Covarrubias <dcov@pm.me>
2020-02-14 09:28:51 -0800

    Added listTopFlavors method to IceCreamShop

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -37,4 +37,27 @@ class IceCreamShop {
         self.toppings = toppings
         self.sizes = sizes
     }
+
+    func listTopFlavors() {
+        let topFlavors = flavors.filter { flavor in flavor.rating > 4.0 }
+        if topFlavors.isEmpty {
+            print("There are no top flavors at this time.")
+            return
+        }
+        
+        var result = ""
+        for (index, flavor) in flavors.enumerated() {
+            if result.isEmpty {
+                result = flavor.name
+            } else {
+                result += ", "
+                if index == flavors.count - 1 {
+                    result += " and "
+                }
+                result += flavor.name
+            }
+        }
+        
+        print("Our top flavors are: \(result)")
+    }
 }
