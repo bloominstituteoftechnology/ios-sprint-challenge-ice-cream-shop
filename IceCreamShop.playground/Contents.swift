60b687cce595da4a827e05be5cb991d9e54f61df
Diego Covarrubias <dcov@pm.me>
2020-02-14 09:22:43 -0800

    Added Cone struct

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -11,3 +11,16 @@ enum Size: Double {
     case large = 7.99
 }
 
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    var fullName: String {
+        "\(flavor.name) ice cream cone \(!topping.isEmpty ? "with " + topping : "")"
+    }
+    
+    func eat() {
+        print("This \(fullName), was delicious!")
+    }
+}
