14d3cf6e037f63687c4f8eeee826f9604a2cd406
Harmony Radley <harmonyradley99@gmail.com>
2020-02-14 13:42:23 -0500

    correcting small erros within sprint

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -35,13 +35,14 @@ class IceCreamShop {
         self.totalSales = totalSales
     }
     
+    
     func listTopFlavors() {
-        var topFlavors: String = ""
-        var ammountTopFlavors: Int = 0
+        var topFlavors = ""
         
         for flavor in flavors {
             if flavor.rating > 4.0 {
-                ammountTopFlavors += 1
+                
+                topFlavors += flavor.name
                 
                 print("Our top flavors are \(topFlavors)")
                 
@@ -53,8 +54,11 @@ class IceCreamShop {
 
 
 func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone? {
-    let newCone = Cone(flavor: <#T##Flavor#>, topping: <#T##String?#>, size: <#T##Size#>)
-    let priceCone = size.rawValue
+    
+    let newCone = Cone(flavor: flavor, topping: topping, size: size)
+    let price = size.rawValue
+    
+     totalSales += price
     
     if let unwrappedTopping = topping {
         print("Your \(flavor.name) ice cream with \(unwrappedTopping) is \(size.rawValue)")
@@ -62,13 +66,9 @@ func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone? {
         print("Your \(flavor.name) ice cream is \(size.rawValue)")
     }
     
-    totalSales += priceCone
     
     return newCone
 }
-
-
-
 
 
 
@@ -77,13 +77,13 @@ let myFlavor = Flavor(name: "Vanilla", rating: 3.5)
 let carlsFlavor = Flavor(name: "Chocolate", rating: 2.3)
 let momsFlavor = Flavor(name: "Cherry", rating: 4.0)
 
-let arrayOfSizes: [Size] = [.small, .medium, .large]
-let arrayOfToppings: [String] = ["sprinkles", "cherry", "whipped cream"]
+let sizes: [Size] = [.small, .medium, .large]
+let toppings: [String] = ["sprinkles", "cherry", "whipped cream"]
 
-let iceCreamShop = IceCreamShop(flavors: [myFlavor], size: arrayOfSizes, toppings: arrayOfToppings, totalSales: 0)
+let newIceCreamShop = IceCreamShop(flavors: [myFlavor], size: sizes, toppings: toppings, totalSales: 0)
 
-iceCreamShop.listTopFlavors()
+newIceCreamShop.listTopFlavors()
 
 newCone?.eat()
 
-print(iceCreamShop.totalSales)
+print(newIceCreamShop.totalSales)
