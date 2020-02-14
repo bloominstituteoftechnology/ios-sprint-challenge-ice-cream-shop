d2ec0d8cd7b8c9eb3a85b27dbbbe7547010fcbe8
Richard Ray Wellman <trys10studios@gmail.com>
2020-02-14 11:24:33 -0600

    working on the next few

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -4,7 +4,7 @@ struct Flavor {
     var rating: Double
 }
 
-enum Size: Double{
+enum Size: Double {
     case small = 8.0
     case medium = 16.0
     case large = 32.0
@@ -12,16 +12,15 @@ enum Size: Double{
 
 struct Cone {
     let flavor: String
-    let topping: String
+    let topping: String? // optional for cone
     let size: Size
 }
 
 class IceCreamShop {
     
-    let flavors: [String] = ["Vanilla", 3.0,"Chocolate","Strawberry"]
+    let flavors: [String] = [Flavor.name = "String","Chocolate","Strawberry"]
     var sizes: [Size] = [.small, .medium, .large]
     var toppings: [String] = ["Sprinkles", "Gummy Bears", "Nuts"]
-    
     let totalSales: Double
     
     init(totalSales: Double, sizes: [Size], toppings: [String]) {
@@ -31,15 +30,45 @@ class IceCreamShop {
     }
 }
 
+var coneString: String = ""
+
+var iceCreamShop: IceCreamShop = IceCreamShop(flavors: [String], sizes: [Size], toppings: [String], totalSales: Double)
+
 let cone = Cone(flavor: "Vanilla", topping: "Sprinkles", size: Size.medium)
 
 func eat() {
     print("Mmm!  I love \(cone.flavor) flavor!")
 }
 
+func orderCone(cone: Cone) {
+    
+    coneString += "Flavor: \(cone.flavor)"
+    
+    if let coneTopping = cone.topping {
+        coneString += " Topping: \(coneTopping)"
+    }
+    
+    coneString += " Size: \(cone.size)"
+    //return cone
+}
+
+
 func listTopFlavors() {
+    
+    let flavors = iceCreamShop.flavors
+    
+    for flavor in flavors {
+        var flavorsString = ""
+            flavorsString += " \(flavor)"
+        print(flavorsString)
+    }
     
 }
 
 
 eat()
+
+orderCone(cone: Cone(flavor: "Vanilla", topping: "Sprinkles", size: .large))
+print(coneString)
+
+listTopFlavors()
