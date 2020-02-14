ef710d9d36977e682c7a8bac1b36d021e6b9a20c
Hector L <hectoremil21@hotmail.com>
2020-02-14 12:44:49 -0500

    Created all objects. Flavor, Size, Cone, IceCreamShop

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
 // Ice Cream Project Hector Ledesma
+
+struct Flavor {
+    var name: String
+    var rating: Int
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String?
+    let size: Size
+}
+
+class IceCreamShop {
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: [String]
+    var totalSales: Int
+    
+    init(flavors: [Flavor], sizes: [Size], toppings: [String]) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+        self.totalSales = 0
+    }
+}
+
