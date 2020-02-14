6ea282c4ebee3a8785d004d8f41c1aba2b49f1ce
Gregory Omoma <omomagreg@yahoo.com>
2020-02-14 12:53:37 -0500

    added a function to struct cone

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,48 @@
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Flavor {
+    let name: String
+    let rating: Int
+    
+    func listTopFlavors() {
+        for flavors i
+    }
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size: String
+    
+    func eat() {
+        print("Mmm, I love \(flavor)")
+    }
+    
+    func orderCone(Cone
+}
+
+class IceCreamShop {
+    var flavors: Flavor
+    var sizes: String
+    var toppings: String
+    var totalSales: Double
+    
+    init(flavors: Flavor, sizes: String, toppings: String, totalSales: Double) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+}
+
+
+//let shopsFlavors: [String] = ["Vanilla", "Chocolate", "Strawberry", "Butter Pecan", "Bubble Gum", "Coffee", "Cookies and Cream"]
+
+
+
+
 
