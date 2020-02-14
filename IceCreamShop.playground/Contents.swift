8ca368b207f59738ecf4455258da44b1712334d5
Lydia Zhang <art4everyan@gmail.com>
2020-02-14 11:37:20 -0500

    Add class and func to it but haven't test it yet

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -11,12 +11,33 @@ struct Flavor {
 }
 
 struct Cone {
-    var flavor: Flavor
+    var flavor: [Flavor]
     var topping: String
     var size: Size
     
     func eat() {
         print("MMM! I love \(self.flavor).")
+    }
+}
+
+class IceCreamShop {
+    var menuItem: [String]
+    var menuType: Cone
+    var totalSales: Double
+    
+    init(menuItem: [String], menuType: Cone, totalSales: Double) {
+        self.menuItem = menuItem
+        self.menuType = menuType
+        self.totalSales = totalSales
+    }
+    func listTopFlavors () {
+        var topFlavor: [String] = []
+        for flavors in menuType.flavor {
+            if flavors.rating > 4.0 {
+                topFlavor.append(flavors.name)
+            }
+        print(topFlavor)
+        }
     }
 }
 
