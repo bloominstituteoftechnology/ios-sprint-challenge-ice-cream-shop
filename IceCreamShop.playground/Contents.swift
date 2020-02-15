eb2b3f531e5ea945357a0325618faa4251f4961e
Richard Ray Wellman <trys10studios@gmail.com>
2020-02-14 18:43:10 -0600

    think I got it done

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -19,6 +19,10 @@ struct Cone {
     var flavor: String
     let size: Size
     let toppings: Toppings
+    
+    func eat() {
+        print("Mmm!  I love \(flavor) flavor!")
+    }
 }
 
 class IceCreamShop {
@@ -26,7 +30,7 @@ class IceCreamShop {
     let cone: Cone
     let sizes: Size
     var toppings: Toppings
-    let totalSales: Double
+    var totalSales: Double
     var flavorArray: [Flavor]
     
     init(cone: Cone, sizes: Size, toppings: Toppings, totalSales: Double, flavorArray: [Flavor]) {
@@ -36,28 +40,9 @@ class IceCreamShop {
         self.totalSales = totalSales
         self.flavorArray = flavorArray
     }
-    func eat() {
-        print("Mmm!  I love \(cone.flavor) flavor!")
-    }
 }
 
 var coneString: String = ""
-
-func orderCone(cone: Cone?) -> String {
-        
-    if let unwrappedCone = cone {
-        coneString += "Flavor: \(unwrappedCone.flavor)"
-    }
-    
-    if let unwrappedConeTopping = cone {
-        coneString += " Topping: \(unwrappedConeTopping.toppings)"
-    }
-    if let unWrappedConeSize = cone {
-        coneString += " Size: \(unWrappedConeSize.size.rawValue)"
-    }
-    
-    return coneString
-}
 
 var flavorVan = Flavor(name: "Vanilla", rating: 3.0)
 var flavorChoc = Flavor(name: "Chocolate", rating: 4.0)
@@ -71,13 +56,13 @@ myFlavorsArray.append(flavorStraw)
 
 var cone1: Cone = Cone(flavor: flavorStraw.name, size: .medium, toppings: .gummybears)
 
-let myIceCreamShop: IceCreamShop = IceCreamShop(cone: cone1, sizes: .medium, toppings: .nuts, totalSales: 0.0, flavorArray: myFlavorsArray)
+var myIceCreamShop: IceCreamShop = IceCreamShop(cone: cone1, sizes: .medium, toppings: .nuts, totalSales: 0.0, flavorArray: myFlavorsArray)
 
 func listTopFlavors(iceCreamShop: IceCreamShop) {
     
     for flavor in iceCreamShop.flavorArray {
-        var flavorsString = flavor.name
-        var flavorRating = flavor.rating
+        let flavorsString = flavor.name
+        let flavorRating = flavor.rating
         if(flavor.rating >= 4.0) {
             print("Our top flavors are: " + flavorsString)
         }
@@ -86,4 +71,32 @@ func listTopFlavors(iceCreamShop: IceCreamShop) {
 
 listTopFlavors(iceCreamShop: myIceCreamShop)
 
+cone1.eat()
+
+func orderCone(cone: Cone?) -> String {
+    
+    if let unwrappedCone = cone {
+        coneString += "Flavor: \(unwrappedCone.flavor)"
+    }
+    
+    if let unwrappedConeTopping = cone {
+        coneString += " Topping: \(unwrappedConeTopping.toppings)"
+    }
+    if let unWrappedConeSize = cone {
+        coneString += " Size: \(unWrappedConeSize.size.rawValue)"
+        myIceCreamShop.totalSales += unWrappedConeSize.size.rawValue
+    }
+    return coneString
+}
+
+func printTotalSales(iceCreamShopTotalSales: IceCreamShop) {
+    print("Total Sales for our Ice Cream Shop: \(myIceCreamShop.totalSales)")
+}
+
 print(orderCone(cone: cone1))
+
+printTotalSales(iceCreamShopTotalSales: myIceCreamShop)
+
+print(orderCone(cone: cone1))
+
+printTotalSales(iceCreamShopTotalSales: myIceCreamShop)
