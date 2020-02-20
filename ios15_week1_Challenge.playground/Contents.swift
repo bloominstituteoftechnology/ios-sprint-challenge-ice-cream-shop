fec61ea8aef7c88472f796ef35ba8b6eed88cf29
solaikulola <israel.aikulola@gmail.com>
2020-02-19 21:26:57 -0500

    sprint challenge

---
+++ ios15_week1_Challenge.playground/Contents.swift
@@ -0,0 +1,94 @@
+import UIKit
+struct Flavor {
+    let city: String
+    let name: String
+    let rate :String
+    let vanilla: String
+    let chocolate: String
+    let strawberry: string
+    let cookiesAndCream : String
+    let rockyRoad : String
+    let chocolateChip: String
+    let MintChocolateChip : String
+    let frenchVanilla: String
+    let coffee: String
+    let peanutButterCup: String
+    
+}
+
+
+enum iceCreamSizes {
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+}
+ 
+struct Cone {
+    let waferCone: String
+    let waffleCone: String
+    let sugarCone: String
+    
+}
+func icecream flavor: String
+      
+class IcecreamShop: String {
+    var flavors: String{
+        let vanilla: String
+        let chocolate: String
+        let strawberry: string
+        let cookiesAndCream : String
+        let rockyRoad : String
+        let chocolateChip: String
+        let MintChocolateChip : String
+        let frenchVanilla: String
+        let coffee: String
+        let peanutButterCup: Strin
+    }
+    var sizes: String {
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+    }
+        var toppings: String {
+        let hotFudge: String
+        let sprinkles: String
+        let caramel: string
+        let oreos: String
+        let peanutButterCup : String
+        let cookieDough: String
+        let whipped : String
+        let hardChocolateCoating: String
+        let meltedMarshMallows: String
+        let cereal: String
+        let nuts: String
+        let fruit: String
+        }
+ // Tracking total sales
+        var TotalSales {
+        let sales: ("daily")
+        let sales2: ("monthly")
+        }
+}
+
+func topFlavors
+    var Chocolate: String
+    var vanilla: String
+    var strawberry: String
+    var cookiesAndCream: String
+
+
+    Func orderCone: string {
+        Var SugarCone: [flavour: vanilla, toppings: hotFudge, size: 4.5]{
+        } else
+        var waffleCone: [flavor: Chocolate, toppings: caramel, size: small]
+        private func updateWithFlavor (flavor:Flavor)
+        }
+    icecreamView.updatewithflavor(flavor)
+    label.text = flavor.name
+}
+        
+}
+
+struct Flavor: String{
+var chocolate = chocolate
+var vanilla = vanilla 
