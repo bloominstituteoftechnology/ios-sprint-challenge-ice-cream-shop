b0c82f22da4f9ac84386158a55b0007a282fe56e
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-13 10:34:44 -0500

    Added Basic Structs, Enums, and a Class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,48 @@
 
+import UIKit
+
+//Enums
+enum Flavors: String {
+    case Orange_Sherbet = "Orange Sherbet"
+    case Vanilla = "Vanilla"
+    case Chocolate = "Chocolate"
+    case Superman = "Superman"
+}
+
+enum Sizes: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+enum Toppings: String {
+    case Sprinkles = "Sprinkles"
+    case MandMs = "M&M's"
+    case Strawberrys = "Strawberrys"
+}
+
+
+//Structs
+struct Flavor {
+    var name: Flavors
+    var rating: Int
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: Toppings
+    let size: Sizes
+    
+    func eat() {
+        print("This \(flavor.name.rawValue) is really good!")
+    }
+}
+
+//Classes
+class IceCreamShop {
+    
+}
+
+/* let cone = Cone(flavor: .init(name: .Vanilla, rating: 4), topping: .Strawberrys, size: .medium) */
+
+
