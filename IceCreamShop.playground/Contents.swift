0f9a507f583a6217c8c448b86b47c14cbd5a7086
Violet Lavender Love <violetlove@Junyper.local>
2020-03-13 11:03:28 -0700

    further ice cream shop updates

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,5 +1,4 @@
-//flavor names
-enum name: String {
+enum Name: String {
     case chocolate
     case vanilla
     case lavenderHoney                  //flavor names
@@ -7,23 +6,17 @@ enum name: String {
     case twist
 }
 
-enum rating: String{
-    case oneStar                            // rating based on 1-3 stars
-    case twoStars
-    case threeStars
-}
-
 struct Flavor{
     let name: Name                         //puts the rating and flavor name together
-    let rating: Rating
+    let rating: Int
 }
 
-enum size {
+enum Size {
     case small(Double)
     case medium(Double)                     //sizes and raw value of prices
     case large(Double)
 }
-enum topping {
+enum Topping {
     case hotFudge
     case caramel
     case sprinkles                             //Toppings
@@ -39,19 +32,23 @@ struct Cone {
 }
 
 func eat() {
-     print("I love \(name.self)!")       // States an ice cream flavor the customer loves!
+     print("I love \(Name.self)!")       // States an ice cream flavor the customer loves!
  }
 
 class IceCreamShop {
     var cone: Cone
     var totalSales = 0.00     //variable for our sales, will be set to increase later
-    var currencyFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()               //currency formatter
-        formatter.numberStyle = .currency
-        return formatter
-    }()
+//    var currencyFormatter: NumberFormatter = {
+//        let formatter = NumberFormatter()               //currency formatter
+//        formatter.numberStyle = .currency
+//        return formatter
+//    }()
     func listTopFlavors(){
-        
+        var flavors : [Flavor] = []
+        for flavor in flavors{
+            
+        }
+                
     }
     func orderCone(flavor: Flavor, cone:Cone){
         let iceCreamCone = Cone(flavor: Flavor, topping: topping, size: size)
