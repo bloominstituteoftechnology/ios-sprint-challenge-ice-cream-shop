9a00efc20fe554e7110f921fa9cd61360bb06817
Nihal Erdal <nihalerdall@gmail.com>
2020-03-13 08:38:18 -0700

    Added step 1,2,3(structs and enums)

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,27 @@
+//1.
 
+struct Flavor {
+    var name : String
+    var rating : Double
+}
+
+
+//2.
+
+enum Size: Double {
+    case Small = 3.0
+    case Medium = 5.0
+    case Large = 8.0
+}
+
+//3.
+
+struct Cone {
+    let flavor : Flavor
+    let topping : String?
+    let size : Size
+    
+    func eat(){
+        print("Mmm! I love \(flavor.name)!")
+    }
+}
