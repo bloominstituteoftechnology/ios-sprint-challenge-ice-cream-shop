ae44cf19cad2e9fe8f01bf3711f710901ae7c535
Joe VeVerka <joeveverka89@gmail.com>
2020-03-13 11:45:42 -0400

    Getting There

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,33 @@
+// Ice-Cream Shop.
+
+enum Flavor {
+    case chocolate
+    case vanilla
+    case strawberry
+}
+
+enum Size: (Double) {
+    case small = 1.99
+    case medium = 2.99
+    case large = 3.99
+}
+
+struct Cone {
+    let flavors: Flavor
+    var toppings: String
+    var size: Size
+}
+
+let myFavCone = Cone(flavors: .chocolate, toppings: "Sprinkles", size: .large)
+func eat() {
+    print("MMM I Love .\(myFavCone.flavors)")
+}
+eat()
+
+class IceCreamShop
+
+
+
+
+
 
