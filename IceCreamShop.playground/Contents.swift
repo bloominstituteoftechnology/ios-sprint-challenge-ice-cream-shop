559fde8e6c267fc1633518ab95622158443ad207
Hunter Oppel <hun.oppel@gmail.com>
2020-03-13 11:59:25 -0400

    Finished the icecreamshop class and added methods for ordering and listing.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,10 +1,11 @@
 struct Flavor {
     let name: String
-    let rating: Int
+    let rating: Double
 }
 
 struct Topping {
     let name: String
+    let price: Double
 }
 
 enum Size: Double {
@@ -15,7 +16,7 @@ enum Size: Double {
 
 struct Cone {
     let flavor: Flavor
-    let topping: Topping
+    let topping: [Topping]?
     let size: Size
     
     func eat(){
@@ -24,13 +25,54 @@ struct Cone {
 }
 
 class IceCreamShop {
+    // Which sizes available won't change but setting the others to var as may want to add more flavors or toppings later
     let sizes: [Size]
-    let flavors: [Flavor]
-    let toppings: [Topping]
+    var flavors: [Flavor]
+    var toppings: [Topping]
+    var totalSales: Double
     
-    init(sizes: [Size], flavors: [Flavor], toppings: [Topping]) {
+    init(sizes: [Size], flavors: [Flavor], toppings: [Topping], totalSales: Double) {
         self.sizes = sizes
         self.flavors = flavors
         self.toppings = toppings
+        self.totalSales = totalSales
+    }
+    
+    func listTopFlavors(){
+        var topFlavors = "Our top flavors are: "
+
+        for flavor in flavors {
+            if flavor.rating > 4.0 {
+                topFlavors += "\(flavor.name) "
+            }
+        }
+        print(topFlavors)
+    }
+    
+    func orderCone(size: Size, flavor: Flavor, toppings: [Topping]?) -> Cone?{
+        let order = Cone(flavor: flavor, topping: toppings, size: size)
+        var orderPrice = size.rawValue
+        
+        var orderString = "Your \(flavor) ice cream "
+        
+        // Adds any amount of toppings to ice cream and price
+        if let toppings = toppings{
+            orderString += "with "
+            for n in 0...toppings.count-1{
+                orderPrice += toppings[n].price
+                if toppings.count > 1 || n == toppings.count-2 {
+                    orderString += "\(toppings[n].name) and "
+                } else if n == toppings.count-1{
+                    orderString += "\(toppings[n].name) "
+                } else {
+                    orderString += "\(toppings[n].name), "
+                }
+            }
+        }
+        
+        orderString += "is \(orderPrice)."
+        totalSales += orderPrice
+        print(orderString)
+        return order
     }
 }
