6e673b1e83bc3a1fdacf5b66aa78ad052e9621e1
Cody Morley <codymorleyinc@gmail.com>
2020-03-13 08:49:44 -0700

    built enum for size and topping; built structs for flavor and cone. commencing work on function 'eat'.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,31 @@
+enum Size: Double {
+    case small = 3.99
+    case medium = 5.99
+    case large = 6.99
+}
+
+enum Topping: String {
+    case chocSprinkles = "chocolate sprinkles"
+    case rainbowSprinkles = "rainbow sprinkles"
+    case nuts = "nuts"
+    case dip = "dipped in chocolate"
+    
+}
+
+struct Flavor {
+    let name: String
+    var rating: Int
+}
+
+struct Cone {
+    let size: Size
+    let flavor: Flavor
+    let topping: String?
+    
+    init(size: Size, flavor: Flavor, topping: Topping) {
+        self.size = size
+        self.flavor = flavor
+        self.topping = topping.rawValue
+    }
+}
 
