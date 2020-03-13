353aa9e557fbca9ec5925d1962ed0f879017376d
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-13 11:20:15 -0500

    Added in Optionals for the cone and changed toppings enum

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -10,18 +10,17 @@ enum Flavors: String, CaseIterable {
     case Superman = "Superman"
 }
 
-enum Sizes: Double {
+enum Sizes: Double, CaseIterable {
     case small = 3.99
     case medium = 4.99
     case large = 5.99
 }
 
-enum Toppings: String {
-    case Sprinkles = "Sprinkles"
-    case MandMs = "M&M's"
-    case Strawberrys = "Strawberrys"
+enum Toppings: Double, CaseIterable {
+    case Sprinkles = 0.25
+    case MandMs = 0.50
+    case Strawberrys = 1.00
 }
-
 
 //Structs
 struct Flavor {
@@ -30,12 +29,19 @@ struct Flavor {
 }
 
 struct Cone {
-    let flavor: Flavor
-    let topping: Toppings
-    let size: Sizes
+    let flavor: Flavor?
+    let topping: Toppings?
+    let size: Sizes?
+    
+    var price: String = "0.00"
     
     func eat() {
-        print("This \(flavor.name.rawValue) is really good!")
+        if let temp_flavor = flavor
+        {
+            print("This \(temp_flavor.name.rawValue) is really good!")
+        } else {
+            print("This cone doesn't have any icecream!")
+        }
     }
 }
 
@@ -50,21 +56,51 @@ class IceCreamShop {
     //Mutable
     var totalSales: Double
     
-    //Functions
-    func listtopFlavors() {
-       // let temp_flavor = Flavors
-        for i in Flavors.allCases {
-            print(i.rawValue)
-        }
-    }
-    
     init(totalSales: Double) {
         self.totalSales = totalSales
     }
 }
 
+var myShop = IceCreamShop(totalSales: 0.00)
+
+//Functions
+func listTopFlavors() {
+    for i in flavor_array {
+        if (i.rating >= 4)
+        {
+            print("\(i.name.rawValue) has a \(i.rating) out of 10 rating!")
+        }
+    }
+}
+
+func orderCone(flavor: Flavor?, topping: Toppings?, size: Sizes?) -> Cone? {
+    
+    var cone: Cone? = Cone(flavor: flavor, topping: topping, size: size)
+    
+    if let temp_size = size {
+        if let temp_topping = topping {
+            cone?.price = String(temp_size.rawValue + temp_topping.rawValue)
+            myShop.totalSales += temp_size.rawValue + temp_topping.rawValue
+        } else {
+            cone?.price = String(temp_size.rawValue)
+            myShop.totalSales += temp_size.rawValue
+        }
+    } else {
+        print("You needs to pick a size before you buy a cone!")
+    }
+    
+    return cone
+}
+
+//Flavor Ratings
+var flavor_array: [Flavor] = []
+flavor_array.append(Flavor(name: .Chocolate, rating: 4))
+flavor_array.append(Flavor(name: .Vanilla, rating: 3))
+flavor_array.append(Flavor(name: .Orange_Sherbet, rating: 9))
+flavor_array.append(Flavor(name: .Superman, rating: 1))
+
+
+listTopFlavors()
 /* let cone = Cone(flavor: .init(name: .Vanilla, rating: 4), topping: .Strawberrys, size: .medium) */
 
 
-var tst = IceCreamShop(totalSales: 5.00)
-tst.listtopFlavors()
