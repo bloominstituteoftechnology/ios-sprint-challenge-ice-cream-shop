c6dedf775e55f79a6c6d9997345d3e82cbae969d
Ronald Huston Jr <rghuston9@gmail.com>
2020-03-13 12:02:00 -0500

    step 3

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -10,3 +10,17 @@ enum Size: Double {
     case medium = 2.50
     case large = 3.19
 }
+
+struct Cone {
+    let flavor: String
+    let topping: [String]?
+    let size: Size
+    
+    func eat() {
+        print("wow. this is really good \(self.flavor) gelato.")
+    }
+}
+let yum = Flavor(name: "lemon", rating: 3.05)
+let twoScoops = Size.medium
+var double = Cone(flavor: "lemon", topping: nil, size: twoScoops)
+double.eat()
