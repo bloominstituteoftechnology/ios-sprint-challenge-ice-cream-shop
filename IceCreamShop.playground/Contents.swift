e90181c338c44c7215e150089b019cf01b500b37
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-03-13 08:42:27 -0700

    Created Flavor, Size, Cone

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,21 @@
 
+struct Flavor {
+    var name: String
+    var rating: Int
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let size: Size
+    let topping: String
+    
+    func eat() {
+        print("Mmm! I love \(flavor.name)")
+    }
+}
