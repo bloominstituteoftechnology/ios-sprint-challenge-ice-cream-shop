9c0cf2b6c4be78c4ba6d4b33cf4cb00cc7facf26
Shawn-James <shawn.james@me.com>
2020-03-13 11:25:58 -0400

    add properties

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,38 @@
+import UIKit
+
+// MARK: - Properties
+struct Flavor {
+    let name: String
+    var rating: Int
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Mmm... I love \(self.flavor.name)")
+    }
+}
+
+class IceCreamShop {
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: [String]
+    var totalSales: Double
+    
+    init(flavors: [Flavor], sizes: [Size], toppings: [String], totalSales: Double = 0.0) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+}
 
