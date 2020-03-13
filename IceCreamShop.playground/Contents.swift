e82a9bbd2b80b94c6c790b816701af979096ab08
Hunter Oppel <hun.oppel@gmail.com>
2020-03-13 11:27:04 -0400

    Generated structs and enums for flavors, toppings, and sizes.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,36 @@
+struct Flavor {
+    let name: String
+    let rating: Int
+}
 
+struct Topping {
+    let name: String
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: Topping
+    let size: Size
+    
+    func eat(){
+        print("Mmm! I love \(flavor)!")
+    }
+}
+
+class IceCreamShop {
+    let sizes: [Size]
+    let flavors: [Flavor]
+    let toppings: [Topping]
+    
+    init(sizes: [Size], flavors: [Flavor], toppings: [Topping]) {
+        self.sizes = sizes
+        self.flavors = flavors
+        self.toppings = toppings
+    }
+}
