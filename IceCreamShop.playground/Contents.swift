056284488c8d9bf59610e88a2a544244a233a5e0
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-03-13 09:58:03 -0700

    Finsihed

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -45,5 +45,44 @@ class IceCreamShop {
     }
     
     
-    
+    func orderCone(flavor: Flavor, topping: String, size: Size) -> Cone? {
+        switch flavor.name {
+        case "Chunky Monkey", "Half Baked", "Rocky Road", "Cherry Garcia":
+            let newCone = Cone(flavor: flavor, size: size, topping: topping)
+            
+            totalSales = newCone.size.rawValue
+            
+            let newString = "Your \(newCone.flavor.name) ice cream with \(newCone.topping) will be \(newCone.size)"
+            
+            return newCone
+        default:
+            print("We dont have that on our menu, we sincerly apologize")
+            break
+        }
+        return nil
+    }
 }
+
+
+let cones = [
+           Cone(flavor: Flavor(name: "Chunky Monkey", rating: 5), size: .small, topping: "Oreos"),
+           Cone(flavor: Flavor(name: "Half Baked", rating: 5), size: .large, topping: "Sprinkles"),
+           Cone(flavor: Flavor(name: "Rocky Road", rating: 5), size: .large, topping: "Chocolate Syrup"),
+           Cone(flavor: Flavor(name: "Cherry Garcia", rating: 5), size: .medium, topping: "Oreos")
+]
+
+let iceCreamShop = IceCreamShop(menu: cones, totalSales: 0)
+iceCreamShop.listTopFlavors()
+
+
+let myCone = cones[1]
+let mySecondBreakfast = cones[2]
+let myElevensies = Cone(flavor: Flavor(name: "Half Baked", rating: 5), size: .large, topping: "Oreos")
+
+iceCreamShop.orderCone(flavor: myCone.flavor, topping: myCone.topping, size: myCone.size)
+iceCreamShop.orderCone(flavor: mySecondBreakfast.flavor, topping: mySecondBreakfast.topping, size: mySecondBreakfast.size)
+iceCreamShop.orderCone(flavor: myElevensies.flavor, topping: myElevensies.topping, size: myElevensies.size)
+
+myCone.eat()
+iceCreamShop.totalSales
+print(iceCreamShop.totalSales)
