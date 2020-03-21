61dcbb7bd4826a8820ac3a4dfe974b8c61e03a41
Clayton Watkins <watkinclayton17@yahoo.com>
2020-03-20 18:18:24 -0700

    Added flavors and toppings. Added Topping Struct.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -11,21 +11,35 @@ enum Size: Double{
     case large = 5.99
 }
 
+struct Toppings{
+    var topping: String
+}
+
 struct Cone{
     var flavor: Flavor
-    var topping: String
+    var topping: Toppings
     var size: Size
 }
 
 class IceCreamShop{
     var menuFlavors: [Flavor]
-    var menuToppings: [String]
+    var menuToppings: [Toppings]
     var totalSales: Double
     
-    init(menuFlavors: [Flavor], menuToppings: [String]){
+    init(menuFlavors: [Flavor], menuToppings: [Toppings]){
         self.menuFlavors = menuFlavors
         self.menuToppings = menuToppings
         self.totalSales = 0.0
     }
-    
 }
+
+// These are the flavors of Ice Cream we have available at our shop!
+let chocolate = Flavor(name: "Chocolate", rating: 8)
+let vanilla = Flavor(name: "Vanilla", rating: 7)
+let cherryChip = Flavor(name: "Cherry Chip", rating: 10)
+let rockyRoad = Flavor(name: "Rocky Road", rating: 9)
+
+// These are the toppings we have available at our shop!
+let fudge = Toppings(topping: "Fudge")
+let peanuts = Toppings(topping: "Peanuts")
+let whippedCream = Toppings(topping: "Whipped Cream")
