e1a7da28fc2766181c95d28bb9ea080df005fd1b
Jimmy Brown <jabro4@gmail.com>
2020-03-21 11:59:21 -0700

    MVP done, spent last hour trying to add an 'and' to separate flavors, no dice

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -17,7 +17,7 @@ struct Cone {
   let size: Size
   
   func eat() {
-    print("Mmm! I love \(flavor.name)")
+    print("Mmm! I love \(flavor.name).")
   }
 }
 
@@ -38,14 +38,14 @@ class IceCreamShop {
   // Top flavors available
   
   func listTopFlavors() {
-    var faveFlavors = " "
+    var faveFlavors = String()
     
     for flavor in flavors {
-      if flavor.rating > 4.0 {
-        faveFlavors += flavor.name
-        print("Our favorite flavors are \(faveFlavors).")
+        if flavor.rating > 4.0 {
+          faveFlavors.append("\(flavor.name) \n")
       }
     }
+    print("Our top flavors are \(faveFlavors)")
   }
 
   func orderCone(flavor: Flavor, topping: String?, size: Size) -> Cone {
@@ -68,12 +68,12 @@ class IceCreamShop {
 
 
 
-
-
+//Flavors
 let chocolate = Flavor(name: "Chocolate", rating: 4.5)
 let vanilla = Flavor(name: "Vanilla", rating: 4.0)
 let strawberry = Flavor(name: "Strawberry", rating: 3.0)
-let chocolateMaltedCrunch = Flavor(name: "Choclate Malted Crunch", rating: 5.0)
+let chocolateMaltedCrunch = Flavor(name: "Chocolate Malted Crunch", rating: 5.0)
+
 
 let toppings = ["sprinkles", "nuts", "Oreos", "fruit"]
 let sizes: [Size] = [.small, .medium, .large]
