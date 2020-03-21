1d1a8f694f6c8355758db30022eec9d3a0c89178
Kenneth Jones <kennethjones1991@outlook.com>
2020-03-20 19:54:08 -0600

    Steps 1, 2, and 3

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,20 @@
+struct Flavor {
+    let name: String
+    let rating: Double
+}
 
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.49
+    case large = 4.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Mmm! I love \(flavor)!")
+    }
+}
