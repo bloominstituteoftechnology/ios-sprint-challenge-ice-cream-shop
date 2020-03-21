6dcc195caa615332c43d397e8ea3d48a48b3a55d
Sam Gutierrez <Samguti02@gmail.com>
2020-03-21 12:59:21 -0600

    Added my IceCreamShop class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -3,8 +3,8 @@
 
 
 struct Flavor {
-    var name: String
-    var rating: String
+    var name: [String]
+    var rating: Double
 }
 
 enum Size: Double {
@@ -14,9 +14,73 @@ enum Size: Double {
 }
 
 struct Cone {
-    let flavor: Flavor
+    let flavor: String
     let topping: String
     let size: Size
 }
 
+var myFavoriteCone = Cone(flavor: "Cookies and Cream", topping: "Crushed Oreos", size: .medium)
 
+func eat() {
+    print("You've chosen the \(myFavoriteCone.flavor) flavor, great choice!")
+}
+
+eat()
+
+
+//var iceCreamFlavors = ["Cookies and Cream", "Chocolate", "Vanilla"]
+//var iceCreamToppings = ["Cookies", "Caramel", "Chocolate Chips"]
+
+var iceCreamFlavor1 = Cone(flavor: "Cookies and Cream", topping: "Caramel", size: .large)
+
+
+struct Sales {
+    var sales: Int
+}
+
+class IceCreamShop {
+    var shopFlavors: Flavor
+    var shopToppings: Cone
+    var shopSizes: Size
+    var totalSales: [Sales]
+    
+    init(shopFlavor: Flavor, shopToppings: Cone, shopSizes: Size) {
+        self.totalSales = []
+        self.shopFlavors = shopFlavor
+        self.shopToppings = shopToppings
+        self.shopSizes = shopSizes
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class IceCreamShop {
+//    var flavors = ["Chocolate", "Vanilla", "Strawberry", "Peanut Butter", "Cookies and Cream", "Cookie Dough"]
+//    var toppings = ["Chocolate Chips", "Oreos", "Cookie Dough Bites", "Tofee Bits", "Caramel", "Cheesecake Bites"]
+//    var sizes: Size
+//    var totalSales: [Int]
+//
+//    init(size: Size) {
+//        self.flavors = []
+//        self.toppings = []
+//        self.sizes = size
+//        self.totalSales = []
+//    }
+//}
+//
+//func listTopFlavors() {
+//
+//}
