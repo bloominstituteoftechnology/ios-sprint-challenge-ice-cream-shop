31d7e7b7a32cea4e34f12a564a023f87e9265732
Clayton Watkins <watkinclayton17@yahoo.com>
2020-03-20 18:11:42 -0700

    Started Playground. Added Structs, enum, and class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,31 @@
 
+
+struct Flavor{
+    var name : String
+    var rating: Int
+}
+
+enum Size: Double{
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone{
+    var flavor: Flavor
+    var topping: String
+    var size: Size
+}
+
+class IceCreamShop{
+    var menuFlavors: [Flavor]
+    var menuToppings: [String]
+    var totalSales: Double
+    
+    init(menuFlavors: [Flavor], menuToppings: [String]){
+        self.menuFlavors = menuFlavors
+        self.menuToppings = menuToppings
+        self.totalSales = 0.0
+    }
+    
+}
