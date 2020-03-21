fac42be12d5421398808463cee92487b6dac8e7e
Sam Gutierrez <Samguti02@gmail.com>
2020-03-21 10:29:20 -0600

    Added Flavor and Cone structs, as well as a Size Enum

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,22 @@
 
+// The app will need to be able to create an ice cream shop, give it a menu of flavors, toppings and ice cream cone sizes and let people order from the menu.
+
+
+struct Flavor {
+    var name: String
+    var rating: String
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+}
+
+
