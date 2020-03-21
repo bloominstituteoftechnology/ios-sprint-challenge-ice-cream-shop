0c0dc59405769495936854e4067b63bacb8bb262
Your Name Here <youremailhere>
2020-03-20 22:01:57 -0400

    I have completed steps 1-3 feel okay so far got stock on number 3. I am not sure if I was suppost to implement struct of flavor into struct of Cones properties

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,30 @@
+//: ## 1.
+struct Flavor {
+    let name: String
+    let rating: Int
+}
+
+//: ## 2.
+enum Size: Double {
+    case small = 3.99
+    case meidum = 5.99
+    case large = 7.99
+}
+
+//: ## 3.
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Mmm! I love \(self.flavor) as my cone flavor!")
+    }
+}
+
+var myFavoriteConeFlavor = Cone(flavor: "Vanilla", topping: "Sprinkles", size: .large)
+
+myFavoriteConeFlavor.eat()
+
+
 
