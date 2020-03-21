102d1104a182469d188447edac77f8361c35a630
Robert Vance <Fearmydotzs@gmail.com>
2020-03-20 21:08:26 -0600

    Completed Sprint Challenge

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -4,9 +4,9 @@ struct Flavor {
 }
 
 enum Size: Double {
-    case small = 3.99
-    case medium = 5.99
-    case large = 6.99
+    case small = 2.99
+    case medium = 4.99
+    case large = 5.99
 }
 
 struct Cone {
@@ -43,9 +43,9 @@ class IceCreamShop {
     }
     func orderCone(flavor: Flavor, topping: String, size: Size) -> Cone? {
         switch flavor.name {
-        case "Oreo", "Reese Pices", "Vanilla", "Chocolate", "Cookie Dough":
+        case "Oreo", "Reese Pieces", "Vanilla", "Chocolate", "Cookie Dough":
             let newCone = Cone(flavor: flavor, topping: topping, size: size)
-            totalSales = newCone.size.rawValue
+            totalSales += newCone.size.rawValue
             let priceString = "Your \(newCone.flavor.name) ice cream with \(newCone.topping) will be\(newCone.size)"
             return newCone
         default:
@@ -57,9 +57,26 @@ class IceCreamShop {
     
 }
 
-let cones = [
-            
+let cones = [Cone(flavor: Flavor(name: "Oreo", rating: 10), topping: "Double Oreo", size: .large),
+             Cone(flavor: Flavor(name: "Reese Pieces", rating: 9), topping: "Oreo", size: .medium),
+             Cone(flavor: Flavor(name: "Vanilla", rating: 5), topping: "Sprinkles", size: .small)]
 
 
 
-]
+let myIceCreamShop = IceCreamShop(menu: cones, totalSales: 0)
+myIceCreamShop.listTopFlavors()
+
+let myCone = cones[0]
+let otherCone = cones[2]
+let myOtherCone = Cone(flavor: Flavor(name: "Reese Pieces", rating: 9), topping: "Oreo", size: .medium)
+
+myIceCreamShop.orderCone(flavor: myCone.flavor, topping: myCone.topping, size: myCone.size)
+myIceCreamShop.orderCone(flavor: otherCone.flavor, topping: otherCone.topping, size: otherCone.size)
+myIceCreamShop.orderCone(flavor: myOtherCone.flavor, topping: myOtherCone.topping, size: myOtherCone.size)
+
+myCone.eat()
+otherCone.eat()
+myOtherCone.eat()
+myIceCreamShop.totalSales
+
+print(myIceCreamShop.totalSales)
