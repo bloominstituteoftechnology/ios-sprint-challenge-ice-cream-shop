fde1932dce2a1a6c1df15690f23202fdd77788cb
Hannah <hannahlbain89@gmail.com>
2020-03-21 12:07:59 -0500

    added some code

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,23 @@
+struct Flavor {
+    var name: String
+    var rating: Int
+    
+}
+
+enum Size {
+    case small
+    case medium
+    case large
+}
+
+let small = 3.99
+let medium = 4.49
+let large = 4.99
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size: String
+}
+
 
