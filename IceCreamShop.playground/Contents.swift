89b4f595ece120c703938c0bb44329a48dff1907
Bronson Mullens <bronsonmullens@icloud.com>
2020-03-20 18:29:21 -0700

    added more properties

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -24,14 +24,20 @@ struct Cone {
 class IceCreamShop {
     var totalSales: Double
     
+    //Ice Cream Flavors
     let chocolate = Flavor(name: "Mud Slide", rating: 4)
     let vanilla = Flavor(name: "Vanilla Cream", rating: 4)
     let bubblegum = Flavor(name: "Bubble Yum", rating: 2)
     let sherbert = Flavor(name: "Fruity Tooti", rating: 5)
+    
+    //Ice Cream Toppings
+    let sprinkles: String = "Sprinkles"
+    let caramelDrizzle: String = "Caramel Drizzle"
+    let gummyWorms: String = "Gummy Worms"
 
     let sizes: Size
     
-    init(totalSales: Double, sizes: Size) {
+    init(totalSales: Double = 0.0, sizes: Size) {
         self.totalSales = totalSales
         self.sizes = sizes
     }
