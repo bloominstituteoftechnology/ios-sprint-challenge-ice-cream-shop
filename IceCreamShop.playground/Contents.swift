443faf34f0a536a8ba11f7c60456083beab37a12
Aaron Peterson <aarondp2011@gmail.com>
2020-03-22 20:28:34 -0500

    Added initial structs, enums, and classes

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,36 @@
+struct Flavor {
+    let name: String
+    let rating: Int
+}
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Mmm! I love \(flavor)")
+    }
+}
+
+class IceCreamShop {
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: [String]
+    var totalSales: Int
+    
+    init(flavors: [Flavor], sizes: [Size], toppings: [String], totalSales: Int) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+}
+
 
