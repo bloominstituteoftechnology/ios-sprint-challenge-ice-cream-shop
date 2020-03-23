06fff266b5d3102afdc6075d116c5dbc3bebcb95
Administrator <administrator@Stuarts-MacBook-Air.local>
2020-03-22 20:38:34 -0500

    Added custom data types

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,34 @@
+import UIKit
 
+struct Flavor {
+    let name: String
+    let rating: Int
+}
+
+enum Size: Double {
+    case small = 1.99
+    case medium = 2.99
+    case large = 3.99
+}
+
+struct cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("MMM! I love \(self.flavor)")
+    }
+}
+
+class IceCreamShop {
+    var flavors: [String]
+    var toppings: [String]
+    var sizes: [Size]
+    
+    init () {
+        self.flavors = ["Strawberry", "Chocolate", "Vanilla"]
+        self.toppings = ["Sprinkles", "Chocolate Chips"]
+        self.sizes = [Size.small, Size.medium, Size.large]
+    }
+}
