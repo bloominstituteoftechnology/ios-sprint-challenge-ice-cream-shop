ff166941704e7d08f61e1d31c4b03e32728afbfb
Administrator <administrator@Stuarts-MacBook-Air.local>
2020-03-22 22:54:06 -0500

    Revised code to make existing functions methods inside custom class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+//    notes contain earlier approaches
+
 struct Flavor {
     let name: String
     let rating: Int
@@ -27,38 +29,76 @@ class IceCreamShop {
     var sizes: [Size]
     var totalSales: Int
     
-    let strawberry = Flavor(name: "Strawberry", rating: 5)
-    let chocolate = Flavor(name: "Chocolate", rating: 5)
-    let vanilla = Flavor(name: "Vanilla", rating: 3)
+//    let strawberry = Flavor(name: "Strawberry", rating: 5)
+//    let chocolate = Flavor(name: "Chocolate", rating: 5)
+//    let vanilla = Flavor(name: "Vanilla", rating: 3)
     
-    init (totalSales: Int) {
-        self.flavors = [strawberry, chocolate, vanilla]
-        self.toppings = ["Sprinkles", "Chocolate Chips"]
-        self.sizes = [Size.small, Size.medium, Size.large]
+    init (flavors: [Flavor], toppings: [String], sizes: [Size], totalSales: Int) {
+        self.flavors = flavors
+        self.toppings = toppings
+//        self.sizes = [Size.small, Size.medium, Size.large]
+        self.sizes = sizes
         self.totalSales = totalSales
     }
-}
-
-func listTopFlavors(_ shop: IceCreamShop) {
     
-    for flavor in shop.flavors {
-        if flavor.rating > 4 {
-            print(flavor.name)
+    func listTopFlavors() {
+        
+        for flavor in self.flavors {
+            if flavor.rating > 4 {
+                print(flavor.name)
+            }
         }
+    }
+    
+    func orderCone(flavor: Flavor, topping: String = "none", size: Size) -> Cone? {
+        let cone = Cone(flavor: flavor, topping: topping, size: size)
+        self.totalSales += Int(cone.size.rawValue)
+        
+        return cone
     }
 }
 
-let dairyQueen = IceCreamShop(totalSales: 250000)
-listTopFlavors(dairyQueen)
+//    func listTopFlavors(_ shop: IceCreamShop) {
+//
+//        for flavor in shop.flavors {
+//            if flavor.rating > 4 {
+//                print(flavor.name)
+//            }
+//        }
+//    }
 
-func orderCone(shop: IceCreamShop, flavor: Flavor, topping: String, size: Size) -> Cone? {
-    let cone = Cone(flavor: flavor, topping: topping, size: size)
-    shop.totalSales += Int(cone.size.rawValue)
-    
-    return cone
-}
+//    let dairyQueen = IceCreamShop(totalSales: 250000)
+//    listTopFlavors(dairyQueen)
 
-print(dairyQueen.totalSales)
-orderCone(shop: dairyQueen, flavor: dairyQueen.chocolate, topping: dairyQueen.toppings[1], size: Size.medium)
-print(dairyQueen.totalSales)
+//    func orderCone(shop: IceCreamShop, flavor: Flavor, topping: String, size: Size) -> Cone? {
+//        let cone = Cone(flavor: flavor, topping: topping, size: size)
+//        shop.totalSales += Int(cone.size.rawValue)
+//
+//        return cone
+//    }
+
+//    print(dairyQueen.totalSales)
+//    orderCone(shop: dairyQueen, flavor: dairyQueen.chocolate, topping: dairyQueen.toppings[1], size: Size.medium)
+//    print(dairyQueen.totalSales)
+
+
+let strawberry = Flavor(name: "Strawberry", rating: 5)
+let chocolate = Flavor(name: "Chocolate", rating: 5)
+let vanilla = Flavor(name: "Vanilla", rating: 3)
+
+let sizes: [Size] = [Size.small, Size.medium, Size.large]
+
+let toppings = ["Sprinkles", "Chocolate Chips"]
+
+let baskinRobbins = IceCreamShop(flavors: [strawberry, chocolate, vanilla], toppings: toppings, sizes: sizes, totalSales: 180000)
+
+baskinRobbins.listTopFlavors()
+
+print(baskinRobbins.totalSales)
+//    baskinRobbins.orderCone(flavor: baskinRobbins.flavors[2], topping: baskinRobbins.toppings[1], size: baskinRobbins.sizes[1])
+//    baskinRobbins.orderCone(flavor: strawberry, size: Size.large)
+let cone = baskinRobbins.orderCone(flavor: strawberry, size: Size.small)
+cone?.eat()
+
+print(baskinRobbins.totalSales)
 
