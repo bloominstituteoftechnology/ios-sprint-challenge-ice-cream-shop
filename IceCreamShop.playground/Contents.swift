c669c77dc5e240bfc865c9b0d6dfbe7a007bacc5
Administrator <administrator@Stuarts-MacBook-Air.local>
2020-03-22 21:11:18 -0500

    Added functions

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -11,7 +11,7 @@ enum Size: Double {
     case large = 3.99
 }
 
-struct cone {
+struct Cone {
     let flavor: Flavor
     let topping: String
     let size: Size
@@ -22,13 +22,43 @@ struct cone {
 }
 
 class IceCreamShop {
-    var flavors: [String]
+    var flavors: [Flavor]
     var toppings: [String]
     var sizes: [Size]
+    var totalSales: Int
     
-    init () {
-        self.flavors = ["Strawberry", "Chocolate", "Vanilla"]
+    let strawberry = Flavor(name: "Strawberry", rating: 5)
+    let chocolate = Flavor(name: "Chocolate", rating: 5)
+    let vanilla = Flavor(name: "Vanilla", rating: 3)
+    
+    init (totalSales: Int) {
+        self.flavors = [strawberry, chocolate, vanilla]
         self.toppings = ["Sprinkles", "Chocolate Chips"]
         self.sizes = [Size.small, Size.medium, Size.large]
+        self.totalSales = totalSales
     }
 }
+
+func listTopFlavors(_ shop: IceCreamShop) {
+    
+    for flavor in shop.flavors {
+        if flavor.rating > 4 {
+            print(flavor.name)
+        }
+    }
+}
+
+let dairyQueen = IceCreamShop(totalSales: 250000)
+listTopFlavors(dairyQueen)
+
+func orderCone(shop: IceCreamShop, flavor: Flavor, topping: String, size: Size) -> Cone? {
+    let cone = Cone(flavor: flavor, topping: topping, size: size)
+    shop.totalSales += Int(cone.size.rawValue)
+    
+    return cone
+}
+
+print(dairyQueen.totalSales)
+orderCone(shop: dairyQueen, flavor: dairyQueen.chocolate, topping: dairyQueen.toppings[1], size: Size.medium)
+print(dairyQueen.totalSales)
+
