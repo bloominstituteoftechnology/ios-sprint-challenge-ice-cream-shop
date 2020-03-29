030ce2b7377daecbf76f7054362cc2bf3326e601
Your Name Here <youremailhere>
2020-03-28 21:50:14 -0400

    more progress

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,48 +1,69 @@
+
+// #1
 struct Flavor {
     var name: String
     var rating: Int
 }
 
+// #2
 enum Size: Double {
     case small = 3.99
     case medium = 4.99
-    case Large = 5.99
+    case large = 5.99
 }
 
+// #3
 struct Cone {
     let flavor: String
     let topping: String
     let size: Size
-}
-/*
-func eat() {
-    for flavor in Cone {
-    var currentFlavor = ""
-        if let flavor = cone.flavor {
-        currentFlavor = flavor
+    
+    func eat() {
+        print ("mmm! I lover \(flavor.name)!")
     }
-print("mmmmmm! I love \(currentFlavor)")
 }
-}
-*/
-// ^^^^FINISH THIS PART LAST? ^^^^^
 
 
+
+// #4
 class IceCreamShop {
     var flavor: [Flavor]
     var size: [Size]
     var toppings: [String]
+    var totalSales: Double
+    
         
-    init(){
-        self.flavor = []
-        self.size = []
-        self.toppings = []
-}
-    func add(flavor: Flavor) {
-        Flavor.append(flavor)
+    init(shopFlavors: [Flavor], shopSizes: [Size], shopToppings:[String]){
+        self.flavor = shopFlavors
+        self.size = shopSizes
+        self.toppings = shopToppings
+        self.totalSales = 0
+        
     }
-}
-//Add total sales here ^^
+    // #5
+    func listTopFlavors() {
+        var topFlavors = [Flavor.name]
+        for flavor in IceCreamShop.flavor {
+            if flavor.rating >= 4 {
+                topFlavors += ("\(flavor)")
+    }
+    print("These are our top flavors: \(topFlavors)")
+            
+    }
+    }
+    // #6
+    func orderCone(orderFlavor: Flavor, orderTopping: string?, orderSize: Size) -> Cone {
+        let order = Cone(flavor: orderFlavor, topping: orderTopping ?? "", size: orderSize)
+        switch orderSize {
+        case .Large:
+            self.totalSales += Size.large.rawValue
+        case .medium:
+            self.totalSales += Size.medium.rawValue
+        case .small:
+            self.totalSales += Size.small.rawValue
+        default:
+            ""
+        }
 
 let flavor1 = Flavor(name: "Chocolate", rating: 1)
 let flavor2 = Flavor(name: "Strawberry", rating: 2)
@@ -52,25 +73,4 @@ let flavor5 = Flavor(name: "Vanilla", rating: 5)
 
 let zachsIcecream = IceCreamShop.self
 
-
-/*
-zachsIcecream.add(flavor: flavor1)
-zachsIcecream.add(flavor: flavor2)
-zachsIcecream.add(flavor: flavor3)
-zachsIcecream.add(flavor: flavor4)
-zachsIcecream.add(flavor: flavor5)
-*/
-//^^^^ IDK WHY THIS WONT WORK^^^
-
-func listTopFlavors() {
-    var topFlavors = [String]
-    for flavor in IceCreamShop.flavor {
-        if let flavor.rating >= 4 {
-            topFlavors += ("\(flavor)")
-}
-print("These are our top flavors: \(topFlavors)")
-}
-}
-
-
-//THIS WAS DIFFICULT
+zachsIcecream.listTopFlavors(IceCreamShop)
