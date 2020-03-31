a183ac4bf0ed019ff290ae86448fccf794764928
Sam Gutierrez <Samguti02@gmail.com>
2020-03-31 17:45:25 -0600

    Finished step 5. Added my Ice cream shop class, and created my topFlavors function

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -3,9 +3,15 @@
 
 
 struct Flavor {
-    var name: [String]
+    var name: String
     var rating: Double
 }
+
+let chocolate = Flavor(name: "Chocolate", rating: 4.2)
+let vanilla = Flavor(name: "Vanilla", rating: 3)
+let cookieDough = Flavor(name: "Cookie Dough", rating: 4.5)
+let cookiesAndCream = Flavor(name: "Cookies and Cream", rating: 5)
+let peanutButter = Flavor(name: "Peanut Butter", rating: 3.9)
 
 enum Size: Double {
     case small = 3.99
@@ -14,71 +20,47 @@ enum Size: Double {
 }
 
 struct Cone {
-    let flavor: String
+    let flavor: Flavor
     let topping: String
     let size: Size
 }
 
-var myFavoriteCone = Cone(flavor: "Cookies and Cream", topping: "Crushed Oreos", size: .medium)
+let orderOne = chocolate
+
 
 func eat() {
-    print("You've chosen the \(myFavoriteCone.flavor) flavor, great choice!")
+    print("You've chosen the \(orderOne.name) flavor, great choice!")
 }
 
 eat()
-
-
-//var iceCreamFlavors = ["Cookies and Cream", "Chocolate", "Vanilla"]
-//var iceCreamToppings = ["Cookies", "Caramel", "Chocolate Chips"]
-
 
 struct Sales {
     var sales: Int
 }
 
 class IceCreamShop {
-    var shopFlavors: Flavor
-    var shopToppings: Cone
-    var shopSizes: Size
-    var totalSales: [Sales]
-    
-    init(shopFlavor: Flavor, shopToppings: Cone, shopSizes: Size) {
+    var shopFlavors = [chocolate, vanilla, cookieDough, cookiesAndCream, peanutButter]
+    var shopToppings = ["Chocolate Chips", "Oreos", "Cookie Dough Bites", "Tofee Bits", "Caramel", "Cheesecake Bites"]
+    var sizes: Size
+    var totalSales: [Int]
+
+    init(sizes: Size) {
         self.totalSales = []
-        self.shopFlavors = shopFlavor
-        self.shopToppings = shopToppings
-        self.shopSizes = shopSizes
+        self.shopFlavors = [chocolate, vanilla, cookieDough, cookiesAndCream, peanutButter]
+        self.shopToppings = ["Chocolate Chips", "Oreos", "Cookie Dough Bites", "Tofee Bits", "Caramel", "Cheesecake Bites"]
+        self.sizes = sizes
     }
 }
 
-IceCreamShop(shopFlavor: .init(name: <#T##[String]#>, rating: <#T##Double#>), shopToppings: <#T##Cone#>, shopSizes: <#T##Size#>)
+func listTopFlavors() -> String {
+    let flavorlist = IceCreamShop(sizes: .large)
+    var topFlavors: [String] = []
+    for flavor in flavorlist.shopFlavors {
+        if flavor.rating >= 4 {
+            topFlavors.append(flavor.name)
+        }
+    }
+    return "Our top flavors are \(topFlavors)"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class IceCreamShop {
-//    var flavors = ["Chocolate", "Vanilla", "Strawberry", "Peanut Butter", "Cookies and Cream", "Cookie Dough"]
-//    var toppings = ["Chocolate Chips", "Oreos", "Cookie Dough Bites", "Tofee Bits", "Caramel", "Cheesecake Bites"]
-//    var sizes: Size
-//    var totalSales: [Int]
-//
-//    init(size: Size) {
-//        self.flavors = []
-//        self.toppings = []
-//        self.sizes = size
-//        self.totalSales = []
-//    }
-//}
-//
-//func listTopFlavors() {
-//
-//}
+print(listTopFlavors())
