1df21abdde4799f4ad3b49538695c5d730e375c0
cedric janssens <cedric-janssens@lambdastudent.com>
2020-04-10 13:43:01 -0400

    vers1.1 Q1-7

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -13,13 +13,47 @@ enum Size: Double{
 
 struct Cone{
     let flavor: Flavor
-    let topping: String
+    let topping: String?
     let size: Size
     
+    init(flavor:Flavor,topping:String,size:Size) {
+        self.flavor = flavor
+        self.topping = topping
+        self.size = size
+    }
     func eat(){
         print("MMM! I love \(flavor)")
     }
 }
+let flavor = Flavor(name: "Pistach", rating: 7)
+let cone = Cone(flavor: flavor, topping: "Choco", size: .large)
+
+class IceCreamShop{
+    var flavor: [Flavor]
+    var size: Size
+    var toppings: String
+    var totalSales: Double
+    
+    init(flavor:[Flavor],size:Size,toppings:String,totalSales:Double) {
+        self.flavor = flavor
+        self.size = size
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+    
+    func listTopFlavors(){
+        var listFlavor: [Flavor]
+        for flavor in listFlavor{
+            if flavor.rating >= 4{
+                listFlavor = listFlavor + flavor.name
+                
+            }
+        }print(listFlavor)
+    }
+}
+
+
+
 
 
 
