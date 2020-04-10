555e37068f99cb1d86507fcb853c2ceb46315d41
cedric janssens <cedric-janssens@lambdastudent.com>
2020-04-10 13:02:25 -0400

    Vers1.0 Q1-3

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,27 @@
 
+struct Flavor{
+    var name: String
+    var rating: Int
+}
+
+
+enum Size: Double{
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone{
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat(){
+        print("MMM! I love \(flavor)")
+    }
+}
+
+
+
+
+
