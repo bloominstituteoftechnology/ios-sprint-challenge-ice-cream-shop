d07f6239864f348649615eb5ac371492ccc719d6
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-04-10 08:56:25 -0700

    Added list top flavors function

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -20,3 +20,29 @@ struct Cone {
         print("Mmm! I love \(flavor.name)")
     }
 }
+
+class IceCreamShop {
+    var menu: [Cone] = []
+    var totalSales: Double
+    
+    init(menu: [Cone], totalSales: Double) {
+        self.menu = menu
+        self.totalSales = totalSales
+    }
+func listTopFlavors() {
+    var greatFlavors = [String]()
+    var topFlavorString = ""
+    
+    for icecream in menu {
+        if icecream.flavor.rating > 4 {
+            greatFlavors.append(icecream.flavor.name)
+            topFlavorString = "Our top flavors are \(greatFlavors.joined(separator: ", "))"
+        } else {
+            print("Sorry we only carry the best Ice Cream here")
+        }
+    }
+    print(topFlavorString)
+    }
+}
+func orderCone(
+
