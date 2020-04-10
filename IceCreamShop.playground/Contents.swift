f910f6b5903bf26d02b733c15ce289fa77ca41a8
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-04-10 15:57:12 -0700

    Additions to Sprint I

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,15 +1,15 @@
 //1.
-struct Flavor {
+struct Flavor: {
     var name: String
     var rating: Double
 }
-
+//the instance of a Flavor, like coffee example
 let flavorOne = Flavor(name: "Strawberry", rating: 5.0)
 let flavorTwo =  Flavor(name: "Cookies and Cream", rating: 4.1)
 let flavorThree = Flavor(name: "Chocolate", rating: 3.1)
 let flavorFour = Flavor(name: "Vanilla Bean", rating: 3.7)
 
-//function for top flavors
+
 
 
 //2.
@@ -27,7 +27,7 @@ struct Cone {
     let size: Size
 
     func eat() {
-        print("Mmm! I love /(flavor.name) ice cream!")
+        print("Mmm! I love /(flavorOne) ice cream!")
     }
     
 }
@@ -36,28 +36,35 @@ struct Cone {
 
 //4.
 class IceCreamShop {
-    var flavor: Flavor
+    var flavor: [Flavor]
     var size: [Size]
     var toppings: [String?]
     var totalSales: Double
+    var rating: Double
     
-    init(flavor: Flavor, size: [Size], toppings: [String?], totalSales: Double) {
+    init(flavor: [Flavor], size: [Size], toppings: [String?], totalSales: Double, rating: Double) {
         
         self.flavor = flavor
         self.size = size
         self.toppings = toppings
         self.totalSales = totalSales
+        self.rating = rating
     }
     func listTopFlavors() {
-        for top in flavor {
-            if flavor.rating >= 4.0 {
-           print("Our top flavor's are \(top.flavor.name).")
+        for top in self.flavor {
+            if self.rating >= 4.0 {
+           print("Our top flavor's are \(_).")
       } else {
-            print("The flavor \(top.flavor.name) is the least popular.")
+            print("The flavor \(_) is the least popular.")
             }
-   
         
  }
+        
+let flavorsShop = IceCreamShop(flavor: "Strawberry", size: "small", toppings: "sprinkles", totalSales: 0)
+
+        
+        
+        
 //5
 
 func orderCone(flavor: Flavor, size: Size, toppings: String?) -> Cone? {
@@ -68,3 +75,40 @@ func orderCone(flavor: Flavor, size: Size, toppings: String?) -> Cone? {
 
     }
 }
+
+
+//7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let newFlavor1 = "Pecan"
+let newFlavor2 = "Peanut Butter"
+let newFlavor3 = "Napoleon"
+let allSizes = ["small", "medium", "large"]
+let newToppings = ["mochi", "gummies", "coconut flakes"]
+
+//8.
+let newIceCreamShop: [Any] = [newFlavor1, newFlavor2, newFlavor3, allSizes, newToppings]
+
+
+
