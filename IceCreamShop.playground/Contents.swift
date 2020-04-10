616a0a40f5aa8b937a882faa5fb4d1ca73908c0f
Ronald Huston Jr <rghuston9@gmail.com>
2020-04-10 10:27:05 -0500

    flavor & size

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,18 @@
+import UIKit
 
+struct Flavor {
+    let name: String
+    let rating: Int
+}
+
+enum Size: Double {
+    case small = 1.89
+    case medium = 2.50
+    case large = 3.19
+}
+
+enum Topping: String {
+    case nuts
+    case hotFudge
+    case sprinkles
+}
