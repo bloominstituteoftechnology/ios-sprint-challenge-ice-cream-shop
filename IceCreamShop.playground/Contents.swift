e5f0c95be7ac0424e98be46d56be4f38f88b3c5d
August Oliver <august@Augusts-MBP.home>
2020-05-08 13:15:33 -0400

    Update to the sprint challenge

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -5,17 +5,69 @@
 
 import Foundation
 
+//struct Flavor {
+//
+//    enum icecreamFlavor {
+//        case chocolate
+//        case vanilla
+//        case strawberry
+//    }
+//    var rating: String
+//}
+//
+//var flavor: Flavor.icecreamFlavor
+//
+//enum Size: Double {
+//    case small
+//    case medium
+//    case large
+//}
+//
+//struct Cone {
+//    let flavor: Flavor.icecreamFlavor
+//
+//    let topping: String
+//    let size: Size
+//
+//    func eat() {
+//        print("Mmm! I love \(flavor)!")
+//    }
+//}
+//
+//class IceCreamShop {
+//    var flavor: Flavor.icecreamFlavor
+//    var size: Size
+//    var toppings: String
+//    var totalSales: Double // keep track of how much money it was made
+//
+//    init(flavor: Flavor.icecreamFlavor, size: Size, toppings: String, totalSales: Double) {
+//        self.flavor = flavor
+//        self.size = size
+//        self.toppings = toppings
+//        self.totalSales = totalSales
+//    }
+//}
+//
+//func listTopFlavors() {
+//
+//}
+
+
+
+
+
+
+
 struct Flavor {
     
-    enum icecreamFlavor {
-        case chocolate
-        case vanilla
-        case strawberry
-    }
-    var rating: String
+    let strawberry: String
+    let vanilla: String
+    let chocolate: String
+    let rating: Double
+    
 }
 
-var flavor: Flavor.icecreamFlavor
+var flavor: Flavor
 
 enum Size: Double {
     case small
@@ -24,26 +76,43 @@ enum Size: Double {
 }
 
 struct Cone {
-    let flavor: Flavor.icecreamFlavor
-    
+    let flavor: Flavor
     let topping: String
     let size: Size
     
     func eat() {
-        print("Mmm! I love \(flavor)!")
+        print("Mmm! I love \(flavor.chocolate)!")
     }
 }
 
 class IceCreamShop {
-    var flavor: Flavor.icecreamFlavor
+    var flavor: Flavor
     var size: Size
     var toppings: String
     var totalSales: Double // keep track of how much money it was made
 
-    init(flavor: Flavor.icecreamFlavor, size: Size, toppings: String, totalSales: Double) {
+    init(flavor: Flavor, size: Size, toppings: String, totalSales: Double) {
         self.flavor = flavor
         self.size = size
         self.toppings = toppings
         self.totalSales = totalSales
     }
 }
+
+
+let ratingsArray = [straberry, chocolate, vanilla]
+
+let straberry = 4.0
+let chocolate = 5.0
+let vanilla = 3.0
+
+
+func listTopFlavors() {
+    for ratings in ratingsArray {
+        if ratings >= 4.0 {
+            print("Our top flavors are: \(ratings)")
+        }
+    }
+}
+
+listTopFlavors()
