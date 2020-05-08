2cc15054f885a9edc5e2c2491227f194153d7ad4
violet-love <violetlove9419@icloud.com>
2020-05-08 09:07:08 -0700

    made a flavor struct with name and rating, made a size enum with prices .25-.75, and created a cone sruct with an eat function

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,29 @@
 import UIKit
+
+struct Flavor {
+    
+    let name: String
+    let rating: Double
+   
+}
+
+enum Size: Double {
+    
+    case small = 0.25
+    case medium = 0.50
+    case large = 0.75
+    
+}
+
+struct Cone {
+    
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+    
+    func eat() {
+        print("Yummy! \(flavor.name) is so good!")
+    }
+    
+}
+
