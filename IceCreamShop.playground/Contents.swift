3bf772b2646148e1f5c2799c1b18b29bc49cdc17
Richard Gibbs <rich-gibbs@lambdastudents.com>
2020-05-08 12:50:05 -0700

    fixed the function that will print top flavors based off of rating

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -4,11 +4,12 @@ struct Flavor {
     let name: String
     let rating: Double
 }
+
 let strawberry = Flavor(name: "Strawberry", rating: 10)
 let chocolate = Flavor(name: "Chocolate", rating: 7)
 let vanilla = Flavor(name: "Vanilla", rating: 6)
 let pistachio = Flavor(name: "Pistachio", rating: 3)
-
+let allFlavors = [strawberry, chocolate, vanilla, pistachio]
 print(strawberry.name)
 
 enum Size: Double {
@@ -29,27 +30,30 @@ struct Cone {
 Cone(size: .large, topping: "Nuts", flavor: chocolate).eat()
 
 class IceCreamShop {
-    var iceCreamFlavors: [Flavor]
+    var chocolateFlavor: Flavor = chocolate
+    var strawberryFlavor: Flavor = strawberry
+    var pistachioFlavor: Flavor = pistachio
+    var vanillaFlavor: Flavor = vanilla
     
-
-    
-    init(iceCreamFlavors: [Flavor]) {
-        self.iceCreamFlavors = iceCreamFlavors
+    init(chocolateFlavor: Flavor, strawberryFlavor: Flavor, pistachioFlavor: Flavor, vanillaFlavor: Flavor) {
+        self.chocolateFlavor = chocolateFlavor
+        self.strawberryFlavor = strawberryFlavor
+        self.pistachioFlavor = pistachioFlavor
+        self.vanillaFlavor = vanillaFlavor
     }
     
-    
+}
     func listTopFlavors() {
-        for topFlavors in iceCreamFlavors {
+        for topFlavors in allFlavors {
             if topFlavors.rating >= 4.0 {
-            print("Our top flavors are: \(topFlavors.name)")
+                print("\(topFlavors.name) is a top flavor.")
         }
     }
 }
-
 
 var cone1 = Cone(size: .medium, topping: "Whipped Cream", flavor: strawberry)
 var cone2 = Cone(size: .small, topping: "Chocolate Sauce", flavor: vanilla)
 var cone3 = Cone(size: .large, topping: "Cherry", flavor: pistachio)
 var cone4 = Cone(size: .medium, topping: " ", flavor: chocolate)
 
-
+listTopFlavors()
