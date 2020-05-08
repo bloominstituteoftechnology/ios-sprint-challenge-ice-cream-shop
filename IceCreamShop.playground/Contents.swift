ad849776c35f7679b460769a2b7f6083dd202555
August Oliver <august@Augusts-MBP.home>
2020-05-08 11:47:43 -0400

    Start of the sprint challenge

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,21 @@
+// Sprint Challenge: Swift Fundamentals - Ice Cream Shop
+// August Oliver
+// May 8th, 2020
 
+
+import Foundation
+
+struct Flavor {
+    var name: String
+    var rating: String
+}
+
+enum Size: Double {
+    case small
+    case medium
+    case large
+}
+
+struct Cone {
+    
+}
