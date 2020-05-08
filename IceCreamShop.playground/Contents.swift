2e4ae9ab517430d442b4c72e719bdb8a1591888f
Ian Becker <ian-becker@lambdastudents.com>
2020-05-08 09:02:19 -0700

    Created Flavor struct, Size enum, Cone struct, and IceCreamShop class.

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,51 @@
-// Ian Becker 
+// Ian Becker
+
+struct Flavor {
+    var name: String
+    var rating: Double
+}
+
+enum Size: Double {
+    case small
+    case medium
+    case large
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String?
+    let size: String
+    
+    func eat() {
+        print("My favorite cone is \(self.flavor).")
+    }
+}
+
+let myFavoriteCone = Cone(flavor: "waffle", topping: "Sprinkles", size: "Small")
+
+myFavoriteCone.eat()
+
+class IceCreamShop {
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: String
+    var totalSales: Double = 3.99
+    
+    init(flavors: [Flavor], sizes: [Size], toppings: String, totalSales: Double) {
+        self.flavors = flavors
+        self.sizes = sizes
+        self.toppings = toppings
+        self.totalSales = totalSales
+    }
+    func listTopFlavors() {
+        for flavor in flavors {
+            if flavor.rating >= 4.0 {
+                print("Our most popular flavors are \(flavors).")
+            }
+        }
+    }
+}
+
+func orderCone(cone: Cone) -> Cone? {
+    let
+}
