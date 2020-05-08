49dc86b81941331a1651dce87cf31ed399b7f7c4
wrscomputers <wrscomputers@gmail.com>
2020-05-08 11:48:46 -0400

    Commit 2
    exit
    quit

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,36 @@
-var firstUpload - " Hello World"
+
+struct Flavor {
+    let name: String
+    let description: String
+    let rating: Int
+    let ingredents: String
+}
+
+enum Size: Double {
+    case kidsCup = 2.49
+    case smallCup = 2.99
+    case mediumCup = 3.99
+    case largeCup = 4.99
+    case kidsCone = 2.69
+    case smallCone = 3.09
+    case mediumCone = 4.09
+    case largeCone = 5.09
+    case waffleCone = 5.49
+}
+
+
+enum SizeDescription: String {
+    case kidsCup = "Kids Cup with Kids Scoop"
+    case smallCup = "Small Cup with 1 Scoop"
+    case mediumCup = "Medium Cup with 2 Scoops"
+    case largeCup = "Large Cup with 3 Scoops"
+    case kidsCone = "Regular Cone with Kids Cone"
+    case smallCone = "Regular Cone with 1 Scoop"
+    case mediumCone = "Regular Cone with 2 Scoops"
+    case largeCone = "Regular Cone with 3 Scoops"
+    case waffleCone = "Waffle Cone with 3 Large Scoops"
+}
+
+struct Cone {
+    
+}
