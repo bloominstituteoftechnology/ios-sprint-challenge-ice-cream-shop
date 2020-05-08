bdd6993b35fceb3b6b61008e509c3f697e7c1df7
Angela Rae Hughes <angelaraehughes@gmail.com>
2020-05-08 10:30:32 -0500

    Sprint initial start

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,40 @@
 
+struct Flavor {
+    var name: String
+    var rating: Int
+    
+}
+
+enum Size {
+    case small
+    case medium
+    case large
+    
+}
+
+struct Cone {
+    var flavor: String
+    var size: String
+        
+}
+
+// func eat
+
+class IceCreamShop {
+    var small: String
+    var medium: String
+    var large: String
+    var chocolate: String
+    var vanilla: String
+    var swirl: String
+    var hotFudge: String
+    var berries:String
+    var totalSales: Double
+    
+}
+
+//func listTopFlavors
+//flavors array top flavors over 4
+
+//func orderCone
+
