67eb06bd46a43975c1950e00777df3eddb726fc4
Mike Nichols <michael-nichols@lambdastudents.com>
2020-05-08 10:20:53 -0500

    1-4 complete

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,38 @@
+// #1
 
+struct Flavor {
+    let name: String
+    var rating: Int?
+}
+
+// #2
+
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+// #3
+
+struct Cone {
+    let flavor: Flavor
+    let topping: String
+    let size: Size
+}
+
+// #4
+
+class IceCreamShop {
+    var menuFlavors: [Flavor]
+    var menuSizes: [Size]
+    var menuToppings: [String]
+    var totalSales: Double
+    
+    init(menuFlavors: [Flavor], menuSizes: [Size], menuToppings: [String]) {
+        self.menuFlavors = menuFlavors
+        self.menuSizes = menuSizes
+        self.menuToppings = menuToppings
+        self.totalSales = 0.0
+    }
+}
