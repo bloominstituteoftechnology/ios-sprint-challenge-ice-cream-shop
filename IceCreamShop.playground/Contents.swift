d30ec51224504f10ed739e53e3d30c165af4ff2d
wrscomputers <wrscomputers@gmail.com>
2020-05-10 14:47:06 -0400

    Commit 2

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,29 @@
- 
+struct Flavor {
+    let name: [String]
+    let rating: Double
+}
+
+enum Size: Double  {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor : Flavor
+    let topping: String
+    let size: Size
+
+    func eat() {
+        print("Mmm! I Love \(flavor)")
+    }
+}
+
+class IceCreamShop {
+    let menu = Cone(flavor: Flavor(name: [flavorsAvailable] , rating: String) , topping: String, size: Size(rawValue: selectedSize))
+    
+}
+
+var selectedSize: String = " "
+
+var flavorsAvailable = [""]
