27e03bdcf533d50cc1c0780edc58d7c4d0cc7095
wrscomputers <wrscomputers@gmail.com>
2020-05-10 15:27:50 -0400

    Commit 3

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,6 +1,8 @@
+import UIKit
+
 struct Flavor {
-    let name: [String]
-    let rating: Double
+    let name: String
+    let rating: Int
 }
 
 enum Size: Double  {
@@ -8,6 +10,8 @@ enum Size: Double  {
     case medium = 4.99
     case large = 5.99
 }
+
+var selectedSize: String
 
 struct Cone {
     let flavor : Flavor
@@ -20,10 +24,51 @@ struct Cone {
 }
 
 class IceCreamShop {
-    let menu = Cone(flavor: Flavor(name: [flavorsAvailable] , rating: String) , topping: String, size: Size(rawValue: selectedSize))
+    let menu = [Cone]()
     
 }
 
-var selectedSize: String = " "
 
-var flavorsAvailable = [""]
+// Flavors
+var flavorsAvailable = [Flavor]()
+
+var flavorOne = Flavor(name: "Vanilla", rating: 3)
+flavorsAvailable.append(flavorOne)
+
+var flavorTwo = Flavor(name: "Neapolitan", rating: 3)
+flavorsAvailable.append(flavorTwo)
+
+var flavorThree = Flavor(name: "Moose Tracks", rating: 3)
+flavorsAvailable.append(flavorThree)
+
+var flavorFour = Flavor(name: "Cherry", rating: 2)
+flavorsAvailable.append(flavorFour)
+
+var flavorFive = Flavor(name: "Strawberry", rating: 3)
+flavorsAvailable.append(flavorFive)
+
+var flavorSix = Flavor(name: "Birthday Cake", rating: 4)
+flavorsAvailable.append(flavorSix)
+
+var flavorSeven = Flavor(name: "Buttered Pecan", rating: 5)
+flavorsAvailable.append(flavorSeven)
+
+var flavorEight = Flavor(name: "Moonshine Butterscotch (Must be 21 years of Age)", rating: 5)
+flavorsAvailable.append(flavorEight)
+
+var flavorNine = Flavor(name: "Cookies & Cream", rating: 5)
+flavorsAvailable.append(flavorNine)
+
+var flavorTen = Flavor(name: "Chocolate", rating: 4)
+flavorsAvailable.append(flavorTen)
+
+var flavorEleven = Flavor(name: "Coffee", rating: 4)
+flavorsAvailable.append(flavorEleven)
+
+var flavorTwelve = Flavor(name: "Banana", rating: 5)
+flavorsAvailable.append(flavorTwelve)
+
+
+
+
+
