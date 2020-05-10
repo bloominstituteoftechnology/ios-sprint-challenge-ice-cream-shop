e60acaccc38f000e15dacf52536db9f0b0a99934
wrscomputers <wrscomputers@gmail.com>
2020-05-10 17:12:57 -0400

    Commit 4

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -15,60 +15,75 @@ var selectedSize: String
 
 struct Cone {
     let flavor : Flavor
-    let topping: String
-    let size: Size
-
+    let topping: String?
+    let sizes: Size
+    
     func eat() {
         print("Mmm! I Love \(flavor)")
     }
 }
 
 class IceCreamShop {
-    let menu = [Cone]()
+    let menu: [Cone]
+    let totalSales: Double
     
+    init(ourMenu: [Cone], sales: Double) {
+        self.menu = ourMenu
+        self.totalSales = sales
+    }
+}
+
+// Flavors
+var flavors = [Flavor]()
+
+var flavorOne = Flavor(name: "Vanilla", rating: 3)
+flavors.append(flavorOne)
+
+var flavorTwo = Flavor(name: "Neapolitan", rating: 3)
+flavors.append(flavorTwo)
+
+var flavorThree = Flavor(name: "Moose Tracks", rating: 3)
+flavors.append(flavorThree)
+
+var flavorFour = Flavor(name: "Cherry", rating: 2)
+flavors.append(flavorFour)
+
+var flavorFive = Flavor(name: "Strawberry", rating: 3)
+flavors.append(flavorFive)
+
+var flavorSix = Flavor(name: "Birthday Cake", rating: 4)
+flavors.append(flavorSix)
+
+var flavorSeven = Flavor(name: "Buttered Pecan", rating: 5)
+flavors.append(flavorSeven)
+
+var flavorEight = Flavor(name: "Moonshine Butterscotch (Must be 21 years of Age)", rating: 5)
+flavors.append(flavorEight)
+
+var flavorNine = Flavor(name: "Cookies & Cream", rating: 5)
+flavors.append(flavorNine)
+
+var flavorTen = Flavor(name: "Chocolate", rating: 4)
+flavors.append(flavorTen)
+
+var flavorEleven = Flavor(name: "Coffee", rating: 4)
+flavors.append(flavorEleven)
+
+var flavorTwelve = Flavor(name: "Banana", rating: 5)
+flavors.append(flavorTwelve)
+
+func listTopFlavors() {
+    print("OUR TOP FLAVORS ARE:")
+    for index in flavors {
+        if index.rating >= 4 {
+            print(index.name)
+        }
+    }
 }
 
 
-// Flavors
-var flavorsAvailable = [Flavor]()
+func orderCone(orderedFlavor: Flavor, orderedTopping: String?, orderedSize: Size) -> Cone? {
+    let order = Cone(flavor: orderedFlavor, topping: orderedTopping, sizes: orderedSize)
 
-var flavorOne = Flavor(name: "Vanilla", rating: 3)
-flavorsAvailable.append(flavorOne)
-
-var flavorTwo = Flavor(name: "Neapolitan", rating: 3)
-flavorsAvailable.append(flavorTwo)
-
-var flavorThree = Flavor(name: "Moose Tracks", rating: 3)
-flavorsAvailable.append(flavorThree)
-
-var flavorFour = Flavor(name: "Cherry", rating: 2)
-flavorsAvailable.append(flavorFour)
-
-var flavorFive = Flavor(name: "Strawberry", rating: 3)
-flavorsAvailable.append(flavorFive)
-
-var flavorSix = Flavor(name: "Birthday Cake", rating: 4)
-flavorsAvailable.append(flavorSix)
-
-var flavorSeven = Flavor(name: "Buttered Pecan", rating: 5)
-flavorsAvailable.append(flavorSeven)
-
-var flavorEight = Flavor(name: "Moonshine Butterscotch (Must be 21 years of Age)", rating: 5)
-flavorsAvailable.append(flavorEight)
-
-var flavorNine = Flavor(name: "Cookies & Cream", rating: 5)
-flavorsAvailable.append(flavorNine)
-
-var flavorTen = Flavor(name: "Chocolate", rating: 4)
-flavorsAvailable.append(flavorTen)
-
-var flavorEleven = Flavor(name: "Coffee", rating: 4)
-flavorsAvailable.append(flavorEleven)
-
-var flavorTwelve = Flavor(name: "Banana", rating: 5)
-flavorsAvailable.append(flavorTwelve)
-
-
-
-
-
+    return order
+}
