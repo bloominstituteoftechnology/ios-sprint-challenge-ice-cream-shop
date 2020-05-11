bd84c129bb403630b8f743fb692b185d8714d9f7
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-11 02:35:39 -0400

    added ordercone function, tweaked class properties

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -31,12 +31,12 @@ enum Size: Double {
 //- Give the struct properties (constants) that hold a flavor, topping (as a `String`), and size.
 //- Create a function called `eat`. It shouldn't have any arguments. It should print a string of text that includes the name of the cone's flavor. For example: "Mmm! I love <flavor here>!"
 struct Cone {
-    let flavor: String
+    let flavor: Flavor
     let topping: String?
-    let size: String
+    let size: Size
     
     func eat() {
-        print("Mmm i love \(self.flavor).")
+        print("Mmm i love \(flavor.name).")
         }                                   //need to finsish
     }
 
@@ -46,31 +46,32 @@ struct Cone {
 //- An ice cream shop needs a menu, so create variables that hold the various flavors, sizes, and toppings you want to have in your shop.
 //- Create a variable called `totalSales`. This will be used to keep track of how much money was made.
 class IceCreamShop {
-    var flavors: [Flavor]         //Flavor
+    var flavors: [Flavor]
     var toppings: String
     var sizes: [Size]
     var totalSales: Double
     
-    init(flavors: [Flavor], toppings: String, sizes: [Size], totalSales: Double) {
+    init(shopFlavors: [Flavor], shopToppings: String, shopSizes: [Size], shopTotalSales: Double) {
         
-        self.flavors = flavors
-        self.toppings = toppings
-        self.sizes = sizes
-        self.totalSales = totalSales
+        flavors = shopFlavors
+        toppings = shopToppings
+        sizes = shopSizes
+        totalSales = shopTotalSales
     }
 
+    
     func listTopFlavors() {
-        for topflavor in flavors {
-            if topflavor.rating => 4.0 {
-                print("Our top flavors are \(flavors)")
-            }
-            
-        }
-    }
+           for topflavor in myShop.flavors {
+               if topflavor.rating >= 4.0 {
+                   let topFlavorsString: String = "Our top flavors are \(topflavor.name)"
+                
+                   print(topFlavorsString)
+               }
+               
+           }
+       }
+    
 }
-
-
-
 
 
 //If a customer asks which flavors are available, we need to be ready to tell them. To accomplish this, do the following:
@@ -78,9 +79,7 @@ class IceCreamShop {
 //- This function doesn't need any arguments.
 //- In the function, build a string that lists out the names of each flavor in the shop's `flavors` array whose rating is over 4.0. For example, the string might say "Our top flavors are <flavors here>." **Hint:** You may want to loop through the array of flavors in order to access each one's name.
 //- When the string has been created, print it.
-
-
-
+ 
 
 
 
@@ -92,55 +91,82 @@ class IceCreamShop {
 //- Add the price of the cone to the `totalSales` variable you made in the previous step.
 //- Create a string that tells the price of the cone, along with its flavor and topping. **NOTE:** account for the potential lack of a topping on the `Cone` in that string by using optional binding (if-let). For example, the string could say "Your mint ice cream with chocolate chips is 3.99", or "Your vanilla ice cream is 5.99." Print the string.
 //- Finally, return the cone you initialized.
-func orderCone(flavor: Flavor, toppings: String?, size: Size) -> Cone? {
- 
-    
-    
-    
-    
-    
-    if let unwrappedTopping = toppings {
-        let orderedCone = Cone(flavor: flavor, topping: toppings, size: size)
+
+func orderCone(topping: String?, flavor: Flavor, size: Size) -> Cone? {
+    let newCone = Cone(flavor: flavor, topping: topping, size: size)
+    myShop.totalSales += newCone.size.rawValue
+    if let toppings = newCone.topping {
+        print("Your \(newCone.flavor.name) ice cream with \(toppings) is \(newCone.size.rawValue)")
+    }
+    return newCone
+}
 
 
 
 
 
+
+
+
+
+//    func orderCone(cone: Cone) -> Cone? {
+//
+//         let newCone = Cone(flavor: cone.flavor, topping: cone.topping, size: cone.size)
+//        totalSales = shopTotalSales + newCone.size.rawValue
+//        guard let unwrappedConeTopping = cone.topping else {
+//            print("Your \(cone.flavor) is \(cone.size)")
+//        }
+//
+//         print("Your \(cone.flavor) with \(unwrappedConeTopping) is \(cone.size)")
+//     }
+
+    
+        
+        
+        
+        
+        
+        
+        
+        // Instances Of Flavors
+    let vanilla = Flavor(name: "Vanilla", rating: 4.0)
+    let chocolate = Flavor(name: "Chocolate", rating: 5.0)
+    let strawberry = Flavor(name: "Strawberry", rating: 3.0)
+
+    
+    
+    
 
 //step8
 //Use the constants you just made to initialize a new `IceCreamShop` constant.
-
-
+let myShop = IceCreamShop(shopFlavors: [vanilla,chocolate,strawberry], shopToppings: "All", shopSizes: [.small, .medium, .large], shopTotalSales: 0.00)
 
 
 
 //step 9
 // Call the shop's `listTopFlavors` function and make sure it runs correctly.
+ 
+myShop.listTopFlavors()
 
 
 //step 10
 //Create a new `Cone` constant. Use the shop's `orderCone` function to assign the constant a `Cone` value.
 
-
+let newCone = orderCone(topping: "Sprinkles", flavor: vanilla, size: .medium)
 
 //step11
 //Using that new `Cone` constant, call its `eat` function without unwrapping the constant.
-
+newCone?.eat()
 
 
 //step 12
 //12. Print the shop's `totalSales` and make sure that it has increased since you ordered a cone in step 10.
 
-
+print(myShop.totalSales)
 
 
 
 ///step 7
 //At the bottom of the playground, create a few `Flavor` constants, an array of sizes, and an array of toppings.
 
-let vanilla = Flavor(name: "Vanilla", rating: 4.0)
-let chocolate = Flavor(name: "Chocolate", rating: 5.0)
-let strawberry = Flavor(name: "Strawberry", rating: 3.0)
-
-let toppings = ["Sprinkle", "Chocolate Sprinkles", "Chopped Penuts", "Gummy Bears"]
-//let sizes: Size = []
+    let shopToppings = ["Sprinkles", "Chocolate Sprinkles", "Choclate Syrup", "Gummy Bears"]
