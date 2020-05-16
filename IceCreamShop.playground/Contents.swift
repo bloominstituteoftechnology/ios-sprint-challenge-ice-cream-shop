ef1adc14f07f63fc179600f6f8a9068abc99aef2
Eugene White <eugenewhite1994@gmail.com>
2020-05-15 20:04:44 -0600

    Working on number 4

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,47 @@
+import UIKit
+
+struct Flavor {
+    let chocolate: String
+    let vanilla: String
+    let mint: String
+    let peanutButter: String
+    
+}
+
+
+
+enum Size: String {
+  case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+    case xl = "Extra Large"
+}
+struct Cone {
+    let waffleCone: String
+    let sprinkles: String
+    let chocolateSyrup: String
+    let topping: String
+}
+ 
+    func eat(flavor: String){
+        print("Mmm! I love \(flavor)")
+}
+
+
+eat(flavor: "Chocolate!")
+
+class IceCreamShop {
+    var menuFlavors = Flavor(chocolate: "Chocolate", vanilla: "Vanilla", mint: "Mint", peanutButter: "Peanut Butter")
+
+    var topping: String
+    
+    
+    init(menuFlavors: Flavor, topping: String) {
+        self.menuFlavors = menuFlavors
+        self.topping = topping
+    }
+        
+    }
+    
+
 
