7b2e5c23647a43324566f6e9a8909b537243d48b
Donna Mayfield <donna-mayfield@lambdastudents.com>
2020-05-17 19:12:25 -0700

    Review flavor variables

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,34 +1,56 @@
 
 struct Flavor {
     var name: String
-    var rating: Double?
+    var rating: Double
 }
 
 enum Size: Double {
-    case small
-    case medium
-    case large
+    case small = 1.50
+    case medium = 2.60
+    case large = 3.50
 }
 
 struct Cone {
-    let flavor: String
+    let flavor: Flavor
     let topping: String
-    let size = Size.RawValue()
+    let size: Size.RawValue
     
     func eat() {
-        print("Yum, I love \(flavor)")
+        print("Yum, I love \(<#_#>)!)"
     }
 }
 
 class IceCreamShop {
-    var totalSales: Double
-    var flavors: [Cone]
+    var totalSales: Size
+    var flavorsMenu: [Flavor]
     
-    init(totalSales: Double, flavors: String) {
+    
+    init(totalSales: Double, flavorsMenu: String) {
         self.totalSales = totalSales
-        self.flavors = []
+        self.flavorsMenu = []
+    }
+
+}
+
+//    Flavor(name: "Triple Cookie Dough", rating: 9.8)
+    var flavor1 = Flavor(name: "Raspberry Gelato", rating: 3.7)
+    var flavor2 = Flavor(name: "Double Choc Fantasy", rating: 10)
+    var flavor3 = Flavor(name: "Caramel Dream", rating: 9.2)
+//
+//    flavorsMenu.
+
+
+func listTopFlavors() {
+    for rating in Cone {
+        if rating > 4.0 {
+            print("Our top flavors are \(Cone.flavor)")
+        }
     }
 }
 
-
-
+func orderCone(flavor: Flavor, topping: String, size: Double) -> Cone? {
+    let myCone: Cone
+    
+    
+    
+}
