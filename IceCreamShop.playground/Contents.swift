55fa8bb56ed268f515afd21a09e68a55d70e35c8
Donna Mayfield <donna-mayfield@lambdastudents.com>
2020-05-17 18:27:59 -0700

    Setup initial enum, struct and class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,34 @@
 
+struct Flavor {
+    var name: String
+    var rating: Double?
+}
+
+enum Size: Double {
+    case small
+    case medium
+    case large
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size = Size.RawValue()
+    
+    func eat() {
+        print("Yum, I love \(flavor)")
+    }
+}
+
+class IceCreamShop {
+    var totalSales: Double
+    var flavors: [Cone]
+    
+    init(totalSales: Double, flavors: String) {
+        self.totalSales = totalSales
+        self.flavors = []
+    }
+}
+
+
+
