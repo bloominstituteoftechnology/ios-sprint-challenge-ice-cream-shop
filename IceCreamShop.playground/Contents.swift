a2a12c5e2877fd94d1e00ae6ead8de871e157a45
Donna Mayfield <donna-mayfield@lambdastudents.com>
2020-05-18 19:56:00 -0700

    revise function scope

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -15,69 +15,69 @@ struct Cone {
     let flavor: Flavor
     let topping: String?
     let size: Size
-    var totalSales: Double = 0.0
     
     func eat() {
         print("Yum, I love \(flavor.name)!)")
     }
 }
 
+
 class IceCreamShop {
     var flavors: [Flavor]
     var sizes: [Size]
     var toppings: [String?]
-
     
     init(flavors: [Flavor], sizes: [Size], toppings: [String?]) {
         self.flavors = flavors
         self.sizes = sizes
         self.toppings = toppings
- //       self.totalSales = totalSales
-    }
-}
-
-
-func listTopFlavors(_ flavors: [Flavor]) {
-    var flavors: [Flavor] = flavors
-    
-    for flavor in flavors {
-        if flavor.rating >= 4.0 {
-            flavors.append(contentsOf: flavors)
-            print("Our top flavors are \(flavor.name)!")
+        
+        
+    func listTopFlavors(_ flavors: [Flavor]) {
+        var flavors: [Flavor] = flavors
+        
+        for flavor in flavors {
+            if flavor.rating >= 4.0 {
+                flavors.append(contentsOf: flavors)
+                print("Our top flavors are \(flavor.name)!")
+            }
         }
     }
-}
 
-func orderCone(_ flavors: Flavor, toppings: String?, size: Size, totalSales: Double) -> Cone? {
-    
-    let myCone = Cone(flavor: flavors, topping: toppings ?? "none", size: Size.RawValue)
-    
-    var _: Double = totalSales + myCone.size
-    
-    
-    if let unwrapTopping = myCone.topping {
-        print("Your \(myCone.flavor) with \(String(describing: myCone.topping)) is \(myCone.size)")
-    } else {
-        print("Your \(myCone.flavor) is \(myCone.size)")
+    func orderCone(_ flavors: Flavor, toppings: String?, sizes: Size, totalSales: Double) -> Cone? {
+        
+        let myCone = Cone(flavor: flavor2, topping: toppings ?? " ", size: sizes)
+ 
+        var totalSales: Double = 0.0
+            totalSales += myCone.size.rawValue
+        
+        if myCone.topping != nil {
+            print("Your \(myCone.flavor) with \(String(describing: myCone.topping)) is \(myCone.size)")
+        } else {
+            print("Your \(myCone.flavor) is \(myCone.size)")
+        }
+        
+        return myCone
+        }
     }
-    
-    return myCone
 }
 
 
 let flavor1 = Flavor(name: "Chocolate Fantasy", rating: 5.8)
 let flavor2 = Flavor(name: "Raspberry Gelato", rating: 3.6)
 
-var myToppings: [String?] = ["Whipped Cream", "Chocolate Whipped Cream", "Banana", "Cherries"]
+var myToppings: [String?] = ["Whipped Cream",
+                             "Chocolate Whipped Cream",
+                             "Banana",
+                             "Cherries"]
+
 var shopSizes: [Size] = [.small, .large, .medium]
 
-let myShop: IceCreamShop
+var myShop: [Flavor]
+myShop.append(flavor1)
+myShop.append(flavor2)
 
-myShop.listTopFlavors()
+let cone1 = Cone(flavor: flavor2, topping: myToppings[1], size: .large)
+cone1.eat()
 
-let cone1 = orderCone(flavors: flavor1, toppings: "", size: .large)
-cone1?.eat()
 print(totalSales)
-
-
-
